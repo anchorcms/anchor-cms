63a9f5b20904b804cb479220af77e0c948427d63
SpenserJ <spenser.e.jones@gmail.com>
2011-05-06 15:44:47 -0600

    Throw a 404 when a controller action is not found

--- index.php
+++ index.php
@@ -5,15 +5,32 @@ require_once 'routes.php';
 require_once 'core/paths.php';
 require_once 'core/class.php';
 require_once 'core/connect.php';
+
+function throw404() {
+  die('KABOOM! Its a 404');
+}
+
 ob_start();
 //include 'loader.php';
 $request = str_replace($urlpath, '', $_SERVER['REQUEST_URI']);
-foreach ($routes as $routeFrom => $routeTo) {
-  if (preg_match('`' . $routeFrom . '`i', $request, $match) == 1) {
-    $route = explode('#', $routeTo);
-    require_once $path . 'controllers/' . $route[0] . '.php';
-    call_user_func(implode('_', $route));
-    break;
+if ($request == '') {
+  $route = explode('#', $root);
+  require_once $path . 'controllers/' . $route[0] . '.php';
+  if (is_callable(($requestFunction = implode('_', $route))) === false) {
+    throw404();
+  }
+  call_user_func($requestFunction);
+} else {
+  foreach ($routes as $routeFrom => $routeTo) {
+    if (preg_match('`' . $routeFrom . '`i', $request, $match) == 1) {
+      $route = explode('#', $routeTo);
+      require_once $path . 'controllers/' . $route[0] . '.php';
+      if (is_callable(($requestFunction = implode('_', $route))) === false) {
+        throw404();
+      }
+      call_user_func($requestFunction);
+      break;
+    }
   }
 }
 $content = ob_get_contents();
