b21563f417f9092d42d0afc3f5f8976d39045d47
SpenserJ <spenser.e.jones@gmail.com>
2011-05-18 11:48:56 -0600

    Settings page now works

--- routes.php
+++ routes.php
@@ -4,6 +4,7 @@ $routes = array(
     'admin/posts' => 'admin_posts#index',
     'admin/posts/new' => 'admin_posts#new',
     'admin/posts/edit/(\d+)' => 'admin_posts#edit',
+    'admin/settings' => 'admin#settings',
     'admin/users' => 'admin_users#index',
     'admin/users/new' => 'admin_users#new',
     'admin/users/edit/(\d+)' => 'admin_users#edit',
