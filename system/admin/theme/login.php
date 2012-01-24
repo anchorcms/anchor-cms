818f5f057a17a2b87cd386b709ca231d1e416b0a
Kieron <info@kieronwilson.co.uk>
2012-01-24 13:04:35 +0000

    tweaks to get the login working

--- system/admin/theme/login.php
+++ system/admin/theme/login.php
@@ -1,11 +1,11 @@
 <?php echo isset($this->error) ? $this->error : ''; ?>
 
-<form action="" method="post">
+<form action="<?php echo $this->get('base_path'); ?>admin/login" method="post">
     <input placeholder="username" name="user">
     <input placeholder="password" name="pass" type="password">
     
     Remember me?
-    <input type="checkbox" name="remember">
+    <input type="checkbox" name="remember" value="1">
    
-    <button name="submit" type="submit">Login</button>
+    <button type="submit">Login</button>
 </form>
