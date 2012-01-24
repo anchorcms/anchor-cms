ef9aac9923511cb644d26c985d415f08c262b13a
Kieron <info@kieronwilson.co.uk>
2012-01-24 13:07:03 +0000

    added some extra info on the post listings

--- theme/default/index.php
+++ theme/default/index.php
@@ -10,10 +10,10 @@
 	<ul class="items">
 		<?php foreach($this->getPosts() as $key => $post): ?>
 		<li>
-		    <a href="/posts/<?php echo $post->slug; ?>" title="<?php echo $post->title; ?>">
-		        <h2><?php echo $post->title; ?></h2>
-		        <p><?php echo $post->description; ?></p>
-		    </a>
+	        <h2><a href="/posts/<?php echo $post->slug; ?>" title="<?php echo $post->title; ?>"><?php echo $post->title; ?></a></h2>
+	        <p>Posted on <?php echo date("jS M, Y", $post->date); ?> by <?php echo $post->author->real_name; ?></p>
+	        <p><?php echo $post->description; ?></p>
+	        <p><a href="/posts/<?php echo $post->slug; ?>">Read Me</a></p>
 		</li>
 		<?php endforeach; ?>
 	</ul>
