764e64e733ff974b0915614f13e4762cfdda9850
Kieron <info@kieronwilson.co.uk>
2012-02-02 20:38:08 +0000

    new default config file to work with the config class

--- config.default.php
+++ config.default.php
@@ -20,49 +20,50 @@
  
 //  Initialise the configuration array
 //  You don't need to change this.
-$config = array();
+return array(
 
-//  Set up the MySQL database
-//  If this doesn't work, you'll need to contact your host.
-$config['database'] = array(
-    
-    //  This is localhost 99% of the time (Dreamhost users: this is usually mysql.domain.com)
-    'host' => 'localhost',
-    
-    //  The username
-    'username' => 'root',
-    
-    //  The password
-    'password' => '',
-    
-    //  The database name
-    'name' => 'anchor'
+	//  Set up the MySQL database
+	//  If this doesn't work, you'll need to contact your host.
+	'database' => array(
+		
+		//  This is localhost 99% of the time (Dreamhost users: this is usually mysql.domain.com)
+		'host' => 'localhost',
+		
+		//  The username
+		'username' => 'root',
+		
+		//  The password
+		'password' => '',
+		
+		//  The database name
+		'name' => 'anchorcms'
+	),
+
+	//  Set metadata information
+	//  This can be accessed from a theme by using $this->get('sitename'), which would grab the sitename, for example.
+	'metadata' => array(
+		'sitename' => 'My First Anchor Site',
+		'description' => 'This is my very cool Anchor CMS site, written in PHP5 and MySQL.',
+		
+		//  The default theme can accept a Typekit account and use their fonts.
+		//  When you get given the script tags to embed, like so:
+		//
+		//  <script type="text/javascript" src="http://use.typekit.com/pfa5tzi.js"></script>
+		//  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+		//
+		//  Extract the random string of text before the ".js", and put it here. (optional, of course)
+		'typekit' => '',
+		
+		//  You can use whatever you want here.
+		'twitter' => '',
+		'date_format' => 'g:i:s A D, F jS Y'
+	),
+
+	//  Set the current theme
+	//  This is a string name of the folder (i.e: $config['theme'] = 'default').
+	'theme' => 'default',
+
+	//  Set debugging options.
+	//  Used for development only. I'd turn this off the live site.
+	'debug' => true
 );
-
-//  Set metadata information
-//  This can be accessed from a theme by using $this->get('sitename'), which would grab the sitename, for example.
-$config['metadata'] = array(
-    'sitename' => 'My First Anchor Site',
-    'description' => 'This is my very cool Anchor CMS site, written in PHP5 and MySQL.',
-    
-    //  The default theme can accept a Typekit account and use their fonts.
-    //  When you get given the script tags to embed, like so:
-    //
-    //  <script type="text/javascript" src="http://use.typekit.com/pfa5tzi.js"></script>
-    //  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
-    //
-    //  Extract the random string of text before the ".js", and put it here. (optional, of course)
-    'typekit' => 'pfa5tzi',
-    
-    //  You can use whatever you want here.
-    'twitter' => '@visualidiot',
-    'date_format' => 'g:i:s A D, F jS Y'
-);
-
-//  Set the current theme
-//  This is a string name of the folder (i.e: $config['theme'] = 'default').
-$config['theme'] = 'default';
-
-//  Set debugging options.
-//  Used for development only. I'd turn this off the live site.
-$config['debug'] = true;
