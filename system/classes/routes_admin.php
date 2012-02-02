79bd1fb4a6901a512afd9fc68571a60b36140d80
Kieron <info@kieronwilson.co.uk>
2012-02-02 23:27:17 +0000

    added post deleteion method

--- system/classes/routes_admin.php
+++ system/classes/routes_admin.php
@@ -32,20 +32,33 @@ class Routes_admin {
 	}
 	
 	public function post_add() {
+		if(Input::method() == 'POST') {
+			if(Posts::add()) {
+				return Response::redirect('admin/posts');
+			}
+		}
 		Template::render('posts/add');
 	}
 	
 	public function post_edit($id = 0) {
+	
+		// find article
 		if(($article = Posts::find(array('id' => $id))) === false) {
 			Notifications::set('notice', 'Post not found');
 			return Response::redirect('admin/posts');
 		}
+		
+		// store object for template functions
 		IoC::instance('article', $article, true);
+		
+		// process post request
 		if(Input::method() == 'POST') {
 			if(Posts::update($id)) {
+				// redirect path
 				return Response::redirect('admin/posts');
 			}
 		}
+
 		Template::render('posts/edit');
 	}
 	
