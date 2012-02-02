e393169505c579332f90a893d58ed66ef0263075
Kieron <info@kieronwilson.co.uk>
2012-02-02 23:14:26 +0000

    missing config check

--- system/run.php
+++ system/run.php
@@ -5,8 +5,7 @@
 */
 if(Config::load() === false) {
 	// looks like we are missing a config file
-	Response::send(PATH . 'system/admin/theme/missing.php', 503);
-	
+	echo file_get_contents(PATH . 'system/admin/theme/missing.php');
 	exit(1);
 }
 
