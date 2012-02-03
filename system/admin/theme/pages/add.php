2f69a8506affab23c1866afeaf4dc810ee714294
Kieron <info@kieronwilson.co.uk>
2012-02-03 21:56:37 +0000

    create a new page template added

---
+++ system/admin/theme/pages/add.php
@@ -0,0 +1,41 @@
+
+<h1>Add Page</h1>
+
+<section class="content">
+
+	<form method="post" action="<?php echo current_url(); ?>">
+		<fieldset>
+			<legend>Details</legend>
+			
+			<?php echo notifications(); ?>
+		
+			<p><label>Name<br>
+			<input name="name" type="text" value="<?php echo Input::post('name'); ?>"></label></p>
+			
+			<p><label>Title<br>
+			<input name="title" type="text" value="<?php echo Input::post('title'); ?>"></label></p>
+			
+			<p><label>Slug<br>
+			<input name="slug" type="text" value="<?php echo Input::post('slug'); ?>"></label></p>
+
+			<p><label>Content<br>
+			<textarea name="content"><?php echo Input::post('content'); ?></textarea></label></p>
+
+			<p><label>Status<br>
+			<select name="status">
+				<?php foreach(array('draft','published','archived') as $status): ?>
+				<option value="<?php echo $status; ?>"<?php if(Input::post('status') == $status) echo 'selected'; ?>>
+					<?php echo ucwords($status); ?>
+				</option>
+				<?php endforeach; ?>
+			</select></label></p>
+		</fieldset>
+
+		<p>
+			<button type="submit">Save</button>
+			<a href="/admin/pages">Return to pages</a>
+		</p>
+	</form>
+
+</section>
+
