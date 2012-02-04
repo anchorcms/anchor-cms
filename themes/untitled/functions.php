04385602126f8fc075fc66c8a1b5399fbf95ca88
Kieron <info@kieronwilson.co.uk>
2012-02-04 02:25:29 +0000

    some custom functions for this theme

--- themes/untitled/functions.php
+++ themes/untitled/functions.php
@@ -1,29 +1,49 @@
 <?php defined('IN_CMS') or die('No direct access allowed.');
 
 /**
-	Custom theme functions - This file will be included for each templae to use
+	Custom theme functions
+	
+	Note: we recommend you prefix all your functions to avoid any naming 
+	collisions or wrap your functions with if function_exists braces.
 */
 
-// counts the number of words that do not contain a vowel
-function count_voweless($str) {
-	$words = preg_split('/\s+/', strip_tags($str), null, PREG_SPLIT_NO_EMPTY);
-	$total = 0;
-	
-	foreach($words as $word) {
-		if(strlen($word) < 3) {
-			continue;
-		}
-		
-		$letters = str_split(strtolower($word));
-		
-		foreach(array('a','e','i','o','u') as $vowel) {
-			if(in_array($vowel, $letters)) {
-				continue 2;
-			}
-		}
-		
-		$total++;
-	}
-	
-	return $total;
+function numeral($number) {
+	$test = abs($number) % 10;
+	$ext = ((abs($number) % 100 < 21 and abs($number) % 100 > 4) ? 'th' : (($test < 4) ? ($test < 3) ? ($test < 2) ? ($test < 1) ? 'th' : 'st' : 'nd' : 'rd' : 'th'));
+	return $number . $ext; 
+}
+
+function count_words($str) {
+	return count(preg_split('/\s+/', strip_tags($str), null, PREG_SPLIT_NO_EMPTY));
+}
+
+function pluralise($amount, $str, $alt = '') {
+    return $amount == 1 ? $str : $str . ($alt !== '' ? $alt : 's');
+}
+
+function relative_time($date) {
+    $elapsed = time() - $date;
+    
+    if($elapsed <= 1) {
+        return 'Just now';
+    }
+    
+    $times = array(
+        31104000 => 'year',
+        2592000 => 'month',
+        604800 => 'week',
+        86400 => 'day',
+        3600 => 'hour',
+        60 => 'minute',
+        1 => 'second'
+    );
+    
+    foreach($times as $seconds => $title) {
+        $rounded = $elapsed / $seconds;
+        
+        if($rounded > 1) {
+            $rounded = round($rounded);
+            return $rounded . ' ' . pluralise($rounded, $title) . ' ago';
+        }
+    }
 }
