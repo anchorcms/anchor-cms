5eff6df76111180b4cbeb8e3ad030306b61ed06e
Kieron <info@kieronwilson.co.uk>
2012-02-05 12:13:39 +0000

    implemented the auto loader finally

--- system/classes/autoload.php
+++ system/classes/autoload.php
@@ -2,10 +2,36 @@
 
 class Autoloader {
 
-	public static function register() {}
+	public static function register() {
+		spl_autoload_register(array('Autoloader', 'load'));
+	}
 	
-	public static function unregister() {}
+	public static function unregister() {
+		spl_autoload_unregister(array('Autoloader', 'load'));
+	}
 	
-	public function load() {}
+	public static function load($class) {
+		$file = str_replace(array('//', '\\'), '/', trim(strtolower($class), '/'));
+
+		// get file path
+		if(($path = static::find($file)) === false) {
+			return false;
+		}
+
+		require $path;
+
+		return true;
+	}
+	
+	public static function find($file) {
+		// search system and application paths
+		foreach(array(PATH . 'system/classes/') as $path) {
+			if(stream_resolve_include_path($path . $file . '.php')) {
+				return $path . $file . '.php';
+			}
+		}
+		
+		return false;
+	}
 
 }
