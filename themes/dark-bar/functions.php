75b018c85da93c9000e0229a00dc87efa6afe7d5
Craig <bertycrazy543@hotmail.co.uk>
2012-02-06 10:20:46 +0000

    Removed get_last_tweet() until further notice.
    Seemed redundant and could not get curl working. Any pointers?

--- themes/dark-bar/functions.php
+++ themes/dark-bar/functions.php
@@ -7,15 +7,6 @@
 	collisions or wrap your functions with if function_exists braces.
 */
 
-function get_last_tweet(){
-	//use the twitter api to get tweet data for the user
-	$json = file_get_contents("https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=".twitter_account()."&count=1"); 
-	//Decode it with JSON
-	$decode = json_decode($json, true);
-	//return tweet text
-	return '<p>My Latest Tweet: ' . $decode[0]['text'] . '</p>';
-}
-
 function numeral($number) {
 	$test = abs($number) % 10;
 	$ext = ((abs($number) % 100 < 21 and abs($number) % 100 > 4) ? 'th' : (($test < 4) ? ($test < 3) ? ($test < 2) ? ($test < 1) ? 'th' : 'st' : 'nd' : 'rd' : 'th'));
