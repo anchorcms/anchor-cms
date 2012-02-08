acd11d5563f9459e8475ef1cbff34cbb09e5411a
rwarasaurus <info@kieronwilson.co.uk>
2012-02-08 21:22:49 +0000

    updated function call

--- themes/default/article.php
+++ themes/default/article.php
@@ -8,6 +8,6 @@
 </section>
 
 <section class="footnote">
-	<p>This article is my <?php echo numeral(article_id() + 1); ?> oldest. It is <?php echo count_words(article_html()); ?> words long. <?php echo custom_field('attribution'); ?></p>
+	<p>This article is my <?php echo numeral(article_id() + 1); ?> oldest. It is <?php echo count_words(article_html()); ?> words long. <?php echo article_custom_field('attribution'); ?></p>
 </section>
 
