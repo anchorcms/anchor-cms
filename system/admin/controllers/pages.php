acabc2eab9eaec2fb4f872d3a30d811520eb0d27
rwarasaurus <info@kieronwilson.co.uk>
2012-02-10 01:51:55 +0000

    redirect to page after add/edit

--- system/admin/controllers/pages.php
+++ system/admin/controllers/pages.php
@@ -14,7 +14,7 @@ class Pages_controller {
 	public function add() {
 		if(Input::method() == 'POST') {
 			if(Pages::add()) {
-				return Response::redirect($this->admin_url . '/pages');
+				return Response::redirect($this->admin_url . '/pages/edit/' . Db::insert_id());
 			}
 		}
 		Template::render('pages/add');
@@ -23,7 +23,6 @@ class Pages_controller {
 	public function edit($id) {
 		// find page
 		if(($page = Pages::find(array('id' => $id))) === false) {
-			Notifications::set('notice', 'Page not found');
 			return Response::redirect($this->admin_url . '/pages');
 		}
 
@@ -31,7 +30,7 @@ class Pages_controller {
 		if(Input::method() == 'POST') {
 			if(Pages::update($id)) {
 				// redirect path
-				return Response::redirect($this->admin_url . '/pages');
+				return Response::redirect($this->admin_url . '/pages/edit/' . $id);
 			}
 		}
 
