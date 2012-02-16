d82702e19eda2d1ded847beddf4ad4bb8eea2d87
rwarasaurus <info@kieronwilson.co.uk>
2012-02-16 00:40:28 +0000

    removed xh profiling

--- index.php
+++ index.php
@@ -6,13 +6,6 @@
  *    Originally built by @visualidiot, with thanks to @kieronwilson, @spenserj and a bunch of other contributors.
  *    You're all great.
  */
- 
-/*
- *	XH Profiling
- */
-if(function_exists('xhprof_enable')) {
-	xhprof_enable(XHPROF_FLAGS_NO_BUILTINS | XHPROF_FLAGS_CPU | XHPROF_FLAGS_MEMORY);
-}
 
 // benchmark
 define('ANCHOR_START', microtime(true));
@@ -28,19 +21,3 @@ define('ANCHOR_VERSION', 0.5);
 
 // Lets bootstrap our application and get it ready to run
 require PATH . 'system/bootstrap.php';
-
-/*
- *	XH Profiling
- */
-if(function_exists('xhprof_enable')) {
-	$xhprof_data = xhprof_disable();
-
-	$path = '../../xh/xhprof_lib/';
-
-	include $path . 'config.php';
-	include $path . 'utils/xhprof_lib.php';
-	include $path . 'utils/xhprof_runs.php';
-
-	$xhprof_runs = new XHProfRuns_Default();
-	$run_id = $xhprof_runs->save_run($xhprof_data, "xhprof_testing");
-}
