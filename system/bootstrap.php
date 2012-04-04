3445d7e4b6a5231508bf56ed6b891176c30ab9eb
rwarasaurus <info@kieronwilson.co.uk>
2012-04-04 10:40:00 +0100

    added more compat checks

--- system/bootstrap.php
+++ system/bootstrap.php
@@ -9,6 +9,53 @@ if(version_compare(PHP_VERSION, '5.3.0', '<')) {
 	exit(1);
 }
 
+/**
+	Register Globals Fix
+*/
+if(ini_get('register_globals')) {
+	$globals = array($_REQUEST, $_SESSION, $_SERVER, $_FILES);
+
+	foreach($globals as $global) {
+		foreach(array_keys($global) as $key) {
+			unset(${$key}); 
+		}
+	}
+}
+
+/**
+	Magic Quotes Fix
+	note: magic quotes is deprecated in PHP 5.3
+	src: php.net/manual/en/security.magicquotes.disabling.php
+*/
+$magic_quotes = function_exists('get_magic_quotes_gpc') and get_magic_quotes_gpc();
+$magic_quotes_sybase = ini_get('magic_quotes_sybase') and strtolower(ini_get('magic_quotes_sybase')) != 'off';
+
+if($magic_quotes or $magic_quotes_sybase) {
+	function strip_slashes_recursive($data) {
+		if(is_array($data)) {
+			foreach ($data as $key => $value) {
+				$data[strip_slashes_recursive($key)] = strip_slashes_recursive($value);
+			}
+		} else {
+			$data = stripslashes($data);
+		}
+		return $data;
+	}
+	$_GET = strip_slashes_recursive($_GET);
+	$_POST = strip_slashes_recursive($_POST);
+	$_COOKIE = strip_slashes_recursive($_COOKIE);
+	$_REQUEST = strip_slashes_recursive($_REQUEST);
+}
+
+// Windows IIS Compatibility  
+if(!isset($_SERVER['REQUEST_URI'])) { 
+	$_SERVER['REQUEST_URI'] = substr($_SERVER['PHP_SELF'], 1); 
+	
+	if(isset($_SERVER['QUERY_STRING'])) { 
+		$_SERVER['REQUEST_URI'] .= '?' . $_SERVER['QUERY_STRING']; 
+	} 
+}
+
 // get our autoloader
 require PATH . 'system/classes/helpers.php';
 require PATH . 'system/classes/autoload.php';
@@ -45,19 +92,6 @@ error_reporting(-1);
 	Error display will be handled by our error class
 */
 ini_safe_set('display_errors', false);
-
-/**
-	Disable magic quotes
-	note: magic quotes is deprecated in PHP 5.3
-	src: php.net/manual/en/security.magicquotes.disabling.php
-*/
-if(function_exists('get_magic_quotes_gpc')) {
-	if(get_magic_quotes_gpc()) {
-		ini_safe_set('magic_quotes_gpc', false);
-		ini_safe_set('magic_quotes_runtime', false);
-		ini_safe_set('magic_quotes_sybase', false);
-	}
-}
 
 /**
 	Check our installation
