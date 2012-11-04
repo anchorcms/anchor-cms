ed3827ba41a8b64b616afaab7c6423d3d5147be6
rwarasaurus <info@kieronwilson.co.uk>
2012-11-04 21:32:49 +0000

    fixed install complete page

--- install/start.php
+++ install/start.php
@@ -11,9 +11,11 @@ Installer::check('Anchor requires <code>pdo_mysql</code> module to be installed.
 	return in_array('mysql', PDO::getAvailableDrivers());
 });
 
-Installer::check('Anchor is already installed!', function() {
-	return is_readable(PATH . 'anchor/config/database.php');
-});
+if(Uri::current() != 'complete') {
+	Installer::check('Anchor is already installed!', function() {
+		return file_exists(PATH . 'anchor/config/database.php') === false;
+	});
+}
 
 if(count(Installer::$errors)) {
 	echo View::make('halt', array('errors' => Installer::$errors))
