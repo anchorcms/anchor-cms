2b200064c727ae5b91dc8a65ca2f57858d743fbf
rwarasaurus <info@kieronwilson.co.uk>
2012-11-04 21:53:26 +0000

    get these urls right

--- install/views/complete.php
+++ install/views/complete.php
@@ -5,7 +5,7 @@
 
 	<p class="options">
 		<a href="<?php echo rtrim(dirname(dirname($_SERVER['SCRIPT_NAME'])), '/') . '/'; ?>index.php/admin" class="button">Visit your admin panel</a>
-		<a href="<?php echo rtrim(dirname(dirname($_SERVER['SCRIPT_NAME'])), '/'); ?>" class="right">Visit your new site</a>
+		<a href="<?php echo rtrim(dirname(dirname($_SERVER['SCRIPT_NAME'])), '/') . '/'; ?>" class="right">Visit your new site</a>
 	</p>
 </section>
 
