0c4ac42798c08e63510bc28fa48600b8ebb16927
rwarasaurus <info@kieronwilson.co.uk>
2012-11-07 18:00:27 +0000

    fixed custom

--- anchor/libraries/template.php
+++ anchor/libraries/template.php
@@ -3,14 +3,19 @@
 class Template extends View {
 
 	public function __construct($template, $vars = array()) {
+		// base path
+		$base = PATH . 'themes' . DS . Config::get('meta.theme') . DS;
+
 		// custom article and page templates
 		if($template == 'page' or $template == 'article') {
 			if($item = Registry::get($template)) {
-				$template .= '-' . $item->slug;
+				if(is_readable($base . $template . '-' . $item->slug . '.php')) {
+					$template .= '-' . $item->slug;
+				}
 			}
 		}
 
-		$this->path = PATH . 'themes' . DS . Config::get('meta.theme') . DS . $template . '.php';
+		$this->path = $base . $template . '.php';
 		$this->vars = array_merge($this->vars, $vars);
 	}
 
