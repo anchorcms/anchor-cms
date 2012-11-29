dc8b322aec36336f702ca29067b21f2434ff9fe9
rwarasaurus <info@kieronwilson.co.uk>
2012-11-29 11:26:30 +0000

    make sure we have a time stamp to work out relative time

--- themes/default/functions.php
+++ themes/default/functions.php
@@ -2,15 +2,15 @@
 
 /**
 	Custom theme functions
-	
-	Note: we recommend you prefix all your functions to avoid any naming 
+
+	Note: we recommend you prefix all your functions to avoid any naming
 	collisions or wrap your functions with if function_exists braces.
 */
 
 function numeral($number) {
 	$test = abs($number) % 10;
 	$ext = ((abs($number) % 100 < 21 and abs($number) % 100 > 4) ? 'th' : (($test < 4) ? ($test < 3) ? ($test < 2) ? ($test < 1) ? 'th' : 'st' : 'nd' : 'rd' : 'th'));
-	return $number . $ext; 
+	return $number . $ext;
 }
 
 function count_words($str) {
@@ -22,12 +22,15 @@ function pluralise($amount, $str, $alt = '') {
 }
 
 function relative_time($date) {
+	// make sure $date is a time stamp
+	if( ! preg_match('/^[0-9]+$/', $date)) $date = strtotime($date);
+
 	$elapsed = time() - $date;
-	
+
 	if($elapsed <= 1) {
 		return 'Just now';
 	}
-	
+
 	$times = array(
 		31104000 => 'year',
 		2592000 => 'month',
@@ -37,10 +40,10 @@ function relative_time($date) {
 		60 => 'minute',
 		1 => 'second'
 	);
-	
+
 	foreach($times as $seconds => $title) {
 		$rounded = $elapsed / $seconds;
-		
+
 		if($rounded > 1) {
 			$rounded = round($rounded);
 			return $rounded . ' ' . pluralise($rounded, $title) . ' ago';
