7de4428c77b6d6ef4377297628f4e206c954c44f
rwarasaurus <info@kieronwilson.co.uk>
2012-12-31 15:55:46 +0000

    restored posts page

--- themes/default/posts.php
+++ themes/default/posts.php
@@ -2,8 +2,9 @@
 
 <section class="content">
 
-	<?php if(has_posts()): posts(); ?>
+	<?php if(has_posts()): ?>
 		<ul class="items">
+			<?php posts(); ?>
 			<li>
 				<article class="wrap">
 					<h1>
@@ -11,20 +12,8 @@
 					</h1>
 
 					<div class="content">
-						<?php echo article_html(); ?>
+						<?php echo Html::markdown(article_html()); ?>
 					</div>
-
-					<footer>
-						Posted <time datetime="<?php echo date(DATE_W3C, article_time()); ?>"><?php echo relative_time(article_time()); ?></time> by <?php echo article_author('real_name'); ?>.
-					</footer>
-				</article>
-			</li>
-			<?php $i = 0; while(posts()): $i++; ?>
-			<li style="background: hsl(215,28%,<?php echo round((($i / posts_per_page()) * 20) + 20); ?>%);">
-				<article class="wrap">
-					<h2>
-						<a href="<?php echo article_url(); ?>" title="<?php echo article_title(); ?>"><?php echo article_title(); ?></a>
-					</h2>
 
 					<footer>
 						Posted <time datetime="<?php echo date(DATE_W3C, article_time()); ?>"><?php echo relative_time(article_time()); ?></time> by <?php echo article_author('real_name'); ?>.
