7b28fb54d2a300155d874b4f5aac60d5a633de1c
rwarasaurus <info@kieronwilson.co.uk>
2013-03-07 14:19:01 +0000

    Doh! forgot the question mark #255

--- anchor/routes/admin/pages.php
+++ anchor/routes/admin/pages.php
@@ -68,7 +68,7 @@ Route::post('admin/pages/edit/(:num)', array('before' => 'auth', 'main' => funct
 	// convert html to entities
 	//$input['content'] = e($input['content']);
 
-	$input['show_in_menu'] = is_null($input['show_in_menu']) 0 : 1;
+	$input['show_in_menu'] = is_null($input['show_in_menu']) ? 0 : 1;
 
 	Page::update($id, $input);
 
@@ -134,7 +134,7 @@ Route::post('admin/pages/add', array('before' => 'auth', 'main' => function() {
 	// convert html to entities
 	//$input['content'] = e($input['content']);
 
-	$input['show_in_menu'] = is_null($input['show_in_menu']) 0 : 1;
+	$input['show_in_menu'] = is_null($input['show_in_menu']) ? 0 : 1;
 
 	$id = Page::create($input);
 
