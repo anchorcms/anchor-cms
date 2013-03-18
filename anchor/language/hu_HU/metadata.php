0118b8e66f1edc94c735413844c2f26524cbcb9e
rwarasaurus <info@kieronwilson.co.uk>
2013-03-18 15:55:45 +0000

    format language folders

---
+++ anchor/language/hu_HU/metadata.php
@@ -0,0 +1,29 @@
+<?php
+
+return array(
+
+	'metadata' => 'Honlap metaadata',
+
+	'sitename' => 'Honlap neve',
+	'sitename_explain' => 'Honlapod neve.',
+	'sitedescription' => 'Honlapod leírása',
+	'sitedescription_explain' => 'Rövid leírás az oldaladról.',
+	'homepage' => 'Főoldal',
+	'homepage_explain' => 'Az aktuális kezdőlap.',
+	'postspage' => 'Bejegyzés oldal',
+	'postspage_explain' => 'Az oldal, ahol megjelennek a bejegyzések.',
+	'posts_per_page' => 'Bejegyzés oldalanként',
+	'posts_per_page_explain' => 'Hány bejegyzés legyen megjelenítve egy oldalon.',
+	'current_theme' => 'Aktuális sablon',
+	'current_theme_explain' => 'Az aktuális sablonod.',
+	'auto_publish_comments' => 'Autómatikus hozzaszólás megjelenítés',
+	'twitter' => 'Twitter',
+	'twitter_explain' => 'Twitter fiókod. Megjelenik így: @<span id="output"></span>.',
+	'save' => 'Változások mentése',
+
+	'missing_sitename' => 'Szükség van az oldal nevére',
+	'missing_sitedescription' => 'Szükség van az oldal leírására',
+	'missing_theme' => 'Szükség van egy sablonra',
+	'meta_success_updated' => 'A metaadatok frissítve'
+
+);
