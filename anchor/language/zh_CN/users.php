e24afa3a44255fecc0165084072402200b439cfa
qiuai <qiuai@vip.qq.com>
2013-04-17 22:09:30 +0800

    add language:zh_CN

---
+++ anchor/language/zh_CN/users.php
@@ -0,0 +1,67 @@
+<?php
+
+return array(
+
+	'users' => '用户',
+
+	'create_user' => '建立新用户',
+	'add_user' => '添加一个新用户',
+	'editing_user' => '编辑 %s&rsquo;s Profile',
+	'remembered' => '我知道我的密码',
+	'forgotten_password' => '忘记密码了?',
+
+	// roles
+	'administrator' => '管理员',
+	'administrator_explain' => '',
+
+	'editor' => '编辑',
+	'editor_explain' => '',
+
+	'user' => '用户',
+	'user_explain' => '',
+
+	// form fields
+	'real_name' => '真实姓名',
+	'real_name_explain' => '',
+
+	'bio' => '简介',
+	'bio_explain' => '',
+
+	'status' => '统计',
+	'status_explain' => '',
+
+	'role' => '角色',
+	'role_explain' => '',
+
+	'username' => '用户名',
+	'username_explain' => '',
+	'username_missing' => '请输入一个不短于 %s 字的用户名',
+
+	'password' => '密码',
+	'password_explain' => '',
+	'password_too_short' => '请输入一个不短于 %s 字的用户名',
+
+	'new_password' => '新密码',
+
+	'email' => '邮件地址',
+	'email_explain' => '',
+	'email_missing' => '请输入一个有效的电子信箱',
+	'email_not_found' => '找不到个人资料.',
+
+	// messages
+	'updated' => '用户资料已更新.',
+	'created' => '用户资料已创建.',
+	'deleted' => '用户资料已删除.',
+	'delete_error' => '您不能删除自己的个人资料',
+	'login_error' => '用户名或密码错误.',
+	'logout_notice' => '您已登出.',
+	'recovery_sent' => '我们已经发送了一封邮件来确认您的密码更改.',
+	'recovery_expired' => '密码恢复令牌已过期，请再试一次.',
+	'password_reset' => '您的新密码已设置,去登录吧!',
+
+	// password recovery email
+	'recovery_subject' => '密码重置',
+	'recovery_message' => '请重置您的密码.' .
+		'要按照下面的链接继续.' . PHP_EOL . '%s',
+
+);
