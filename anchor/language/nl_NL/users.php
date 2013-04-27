aec4a0e1322cb88e5dc9babe2a5ae50bb372281b
Janyk Steenbeek <github@janyksteenbeek.nl>
2013-04-27 21:21:07 +0200

    Dutch translation for AnchorCMS 0.9

---
+++ anchor/language/nl_NL/users.php
@@ -0,0 +1,68 @@
+<?php
+// nl_NL translation for AnchorCMS by www.janyksteenbeek.nl
+
+return array(
+
+	'users' => 'Gebruikers',
+
+	'create_user' => 'Maak een nieuwe gebruiker aan',
+	'add_user' => 'Voeg een gebruiker toe',
+	'editing_user' => '%s&rsquo;s zijn profiel wordt aangepast',
+	'remembered' => 'Ik weet mijn wachtwoord',
+	'forgotten_password' => 'Wachtwoord vergeten?',
+
+	// roles
+	'administrator' => 'Admin',
+	'administrator_explain' => '',
+
+	'editor' => 'Editor',
+	'editor_explain' => '',
+
+	'user' => 'User',
+	'user_explain' => '',
+
+	// form fields
+	'real_name' => 'Echte naam',
+	'real_name_explain' => '',
+
+	'bio' => 'Biografie',
+	'bio_explain' => '',
+
+	'status' => 'Status',
+	'status_explain' => '',
+
+	'role' => 'Rol',
+	'role_explain' => '',
+
+	'username' => 'Gebruikersnaam',
+	'username_explain' => '',
+	'username_missing' => 'Voer aub een gebruikersnaam in die minimaal %s tekens bevat',
+
+	'password' => 'Wachtwoord',
+	'password_explain' => '',
+	'password_too_short' => 'Je wachtwoord moet minimaal %s tekens lang zijn',
+
+	'new_password' => 'Nieuwe wachtwoord',
+
+	'email' => 'Email',
+	'email_explain' => '',
+	'email_missing' => 'Voer een geldig e-mail adres',
+	'email_not_found' => 'Profiel niet gevonden.',
+
+	// messages
+	'updated' => 'Gebruiker geupdate.',
+	'created' => 'Gebruiker aangemaakt.',
+	'deleted' => 'Gebruiker verwijderd.',
+	'delete_error' => 'Je kan je eigen profiel niet verwijderen',
+	'login_error' => 'Gebruikersnaam of wachtwoord incorrect.',
+	'logout_notice' => 'Je bent uitgelogd.',
+	'recovery_sent' => 'We hebben je een bevestigings e-mail verzonden!',
+	'recovery_expired' => 'Wachtwoord herstel token is verlopen, probeer opnieuw',
+	'password_reset' => 'Je wachtwoord is veranderd! Log snel in!',
+
+	// password recovery email
+	'recovery_subject' => 'Wachtwoord herstel',
+	'recovery_message' => 'Je hebt een aanvraag gestuurd om je wachtwoord te veranderen.' .
+		'Gebruik deze link om je wachtwoord te veranderen.' . PHP_EOL . '%s',
+
+);
