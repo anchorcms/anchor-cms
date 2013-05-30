b2dff7183dcf55a06ab3a27c469041d4afea790a
rwarasaurus <info@kieronwilson.co.uk>
2013-05-30 21:11:13 +0100

    focus mode be gone dammit!

--- anchor/views/partials/editor.php
+++ anchor/views/partials/editor.php
@@ -1,15 +1,14 @@
 <section class="editor">
 	<div class="wrap">
-    	<nav>
-        	<a class="icon bold" href="#bold">Bold</a>
-        	<a class="icon italic" href="#italic">Italic</a>
-        	<a class="icon list" href="#list">List</a>
-        	<a class="icon quote" href="#quote">Quote</a>
-        	<a class="icon code" href="#code">Code</a>
-        	<a class="icon link" href="#link">Link</a>
-        </nav>
-        
-        <a class="focus">Focus mode</a>
-        <a class="moar" href="#meta">Show more options</a>
+		<nav>
+			<a class="icon bold" href="#bold">Bold</a>
+			<a class="icon italic" href="#italic">Italic</a>
+			<a class="icon list" href="#list">List</a>
+			<a class="icon quote" href="#quote">Quote</a>
+			<a class="icon code" href="#code">Code</a>
+			<a class="icon link" href="#link">Link</a>
+		</nav>
+
+		<a class="moar" href="#meta">Show more options</a>
 	</div>
 </section>
