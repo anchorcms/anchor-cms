cdaabd1a82a4fc61d9a1bebfaa17c956d15ee75f
Garrett Johnson <garrettrayj@gmail.com>
2013-06-16 15:18:52 -0500

    Added textarea resize script to add and edit views for pages

--- anchor/views/pages/add.php
+++ anchor/views/pages/add.php
@@ -87,6 +87,7 @@
 <script src="<?php echo asset('anchor/views/assets/js/redirect.js'); ?>"></script>
 <!--<script src="<?php echo asset('anchor/views/assets/js/focus-mode.js'); ?>"></script>-->
 <script src="<?php echo asset('anchor/views/assets/js/upload-fields.js'); ?>"></script>
+<script src="<?php echo asset('anchor/views/assets/js/text-resize.js'); ?>"></script>
 <script src="<?php echo asset('anchor/views/assets/js/editor.js'); ?>"></script>
 <script>
 	$('textarea[name=content]').editor();
