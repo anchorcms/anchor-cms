83d8558e9a4d61cad5d99fa93c993bef9a143729
Lance <indigofeather@gmail.com>
2013-06-23 02:43:29 +0800

    Added Taiwan language

---
+++ anchor/language/zh_Hant_TW/global.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * Global is to replace common.php
+ *
+ * This global file will provide the majority of the translations in anchor
+ * to remove duplicate translations in section specific language files.
+ */
+return array(
+
+	// words
+	'save' => '儲存',
+	'delete' => '刪除',
+	'update' => '更新',
+	'edit' => '編輯',
+	'editing' => '編輯',
+	'create' => '建立',
+	'created' => '建立',
+	'submit' => '送交',
+	'close' => '關閉',
+	'status' => '狀態',
+	'manage' => '管理',
+	'reset' => '重設',
+	'all' => '全部',
+
+	// pagination
+	'next' => '下一頁',
+	'previous' => '上一頁',
+	'first' => '第一頁',
+	'last' => '最後一頁',
+
+	// statuses
+	'draft' => '草稿',
+	'archived' => '封存',
+	'published' => '公佈',
+	'pending' => '等待',
+	'approved' => '通過',
+	'spam' => '垃圾',
+
+	'inactive' => '未啟用',
+	'active' => '啟用',
+
+	// roles
+	'administrator' => '管理者',
+	'editor' => '編輯者',
+	'user' => '使用者',
+
+	'log_in' => '登入',
+	'login' => '登入',
+	'log_out' => '登出',
+	'logout' => '登出',
+
+	// pharses
+	'visit_your_site' => '造訪你的網站',
+	'powered_by_anchor' => '由 Anchor 驅動，版本 %s',
+	'make_blogging_beautiful' => '讓部落格更美好。',
+
+	// intro
+	'welcome_to_anchor' => '歡迎來到 Anchor',
+	'welcome_to_anchor_lets_go' => '歡迎來到 Anchor。我們走吧。',
+	'run_the_installer' => '執行安裝程序',
+
+	// upgrade
+	'upgrade' => '升級',
+	'good_news' => '好消息！',
+	'new_version_available' => '有可用的 anchor 新版本。',
+	'download_now' => '立即下載',
+	'upgrade_later' => '稍後升級',
+
+	// debug profiler
+	'profile' => '分析',
+	'profile_memory_usage' => '記憶體總使用量',
+
+	// messages
+	'confirm_delete' => '確定刪除？此操作不能還原！'
+
+);
---
+++ anchor/language/zh_Hant_TW/users.php
@@ -0,0 +1,67 @@
+<?php
+
+return array(
+
+	'users' => '使用者',
+
+	'create_user' => '建立新使用者',
+	'add_user' => '新增使用者',
+	'editing_user' => '編輯 %s 的個人資料',
+	'remembered' => '我知道我的密碼',
+	'forgotten_password' => '忘記密碼？',
+
+	// roles
+	'administrator' => '管理者',
+	'administrator_explain' => '',
+
+	'editor' => '編輯者',
+	'editor_explain' => '',
+
+	'user' => '使用者',
+	'user_explain' => '',
+
+	// form fields
+	'real_name' => '名稱',
+	'real_name_explain' => '',
+
+	'bio' => '個人簡歷',
+	'bio_explain' => '',
+
+	'status' => '狀態',
+	'status_explain' => '',
+
+	'role' => '角色',
+	'role_explain' => '',
+
+	'username' => '使用者名稱',
+	'username_explain' => '',
+	'username_missing' => '請輸入使用者名稱，至少 %s 個字元',
+
+	'password' => '密碼',
+	'password_explain' => '',
+	'password_too_short' => '密碼至少 %s 個字元',
+
+	'new_password' => '新密碼',
+
+	'email' => 'Email',
+	'email_explain' => '',
+	'email_missing' => '請輸入有效的 Email 地址',
+	'email_not_found' => '找不到個人資料。',
+
+	// messages
+	'updated' => '使用者個人資料已更新。',
+	'created' => '使用者個人資料已建立。',
+	'deleted' => '使用者個人資料已刪除。',
+	'delete_error' => '不能刪除自己的個人資料',
+	'login_error' => '使用者名稱或密碼錯誤。',
+	'logout_notice' => '現在你已經登出。',
+	'recovery_sent' => '已發送確認密碼變更的信件。',
+	'recovery_expired' => '密碼復原符記已過期，請重試。',
+	'password_reset' => '新密碼已經設定。立即登入！',
+
+	// password recovery email
+	'recovery_subject' => '重設密碼',
+	'recovery_message' => '你請求重設密碼。' .
+		'跟隨以下連結繼續。' . PHP_EOL . '%s',
+
+);
