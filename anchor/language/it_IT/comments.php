54a847d1cfdfbe11c43e18a379c131d843d6ae99
Ludo237 <ludovico1990@hotmail.it>
2013-12-05 10:06:36 +0100

    [Add it_IT lang]
    Italian Language Added

---
+++ anchor/language/it_IT/comments.php
@@ -0,0 +1,35 @@
+<?php
+
+return array(
+
+	'comments' => 'Commenti',
+	'nocomments_desc' => 'Nessun commento.',
+	'editing_comment' => 'Modifica il commento',
+	'view_comment' => 'Visualizza il commento',
+
+	// form fields
+	'name' => 'Nome',
+	'name_explain' => 'Nome dell autore',
+	'name_missing' => 'Perfavore inserisci un nome',
+
+	'email' => 'Indirizzo Email',
+	'email_explain' => 'Email dell autore',
+	'email_missing' => 'Perfavore inserisci un Email valida', // frontend message (appears on your site!)
+
+	'text' => 'Commento',
+	'text_explain' => '',
+	'text_missing' => 'Perfavore inserisci il testo del commento', // frontend message (appears on your site!)
+
+	'status' => 'Stato',
+	'status_explain' => '',
+
+	// messages
+	'created' => 'Il tuo commento è stato aggiunto', // frontend message (appears on your site!)
+	'updated' => 'Il tuo commento è stato aggiornato',
+	'deleted' => 'Il tuo commento è stato eliminato',
+
+	// email notification
+	'notify_subject' => 'Un nuovo commento è stato aggiunto',
+	'nofity_heading' => 'E\' stato aggiunto un nuovo commento sul tuo sito web.'
+
+);
