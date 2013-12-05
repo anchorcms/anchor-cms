757fd8dd869460a6ad13590c83fbeaae19572e97
Ludo237 <ludovico1990@hotmail.it>
2013-12-05 10:46:32 +0100

    [Update it_IT]
    Update language files

---
+++ anchor/language/it_IT/pages.php
@@ -0,0 +1,45 @@
+<?php
+
+return array(
+
+	'pages' => 'Pagine',
+
+	'create_page' => 'Crea una nuova pagina',
+	'nopages_desc' => 'Non hai nessuna pagina.',
+	'redirect' => 'Reindirizzamento',
+
+	// form fields
+	'redirect_url' => 'URL di Reindirizzamento',
+	'redirect_missing' => 'Perfavore inserisci un URL valido.',
+
+	'title' => 'Titolo Pagina',
+	'title_explain' => '',
+	'title_missing' => 'Perfavore inserisci un titolo',
+
+	'content' => 'Contenuto',
+	'content_explain' => 'Il contenuto della tua pagina. Usa il MarkDown.',
+
+	'show_in_menu' => 'Visualizza nel Menu',
+	'show_in_menu_explain' => '',
+
+	'name' => 'Nome',
+	'name_explain' => '',
+
+	'slug' => 'Slug',
+	'slug_explain' => 'URI Slug per identificare la tua pagina, dovrebbe contenere solo caratteri ASCII',
+	'slug_missing' => 'Perfavore inserisci un URI Slug, dovrebbe contenere solo caratteri ASCII',
+	'slug_duplicate' => 'Questo Slug già esiste',
+	'slug_invalid' => 'Lo Slug deve contenere delle lettere',
+
+	'status' => 'Stato',
+	'status_explain' => '',
+
+	'parent' => 'Genitore',
+	'parent_explain' => '',
+
+	// messages
+	'updated' => 'La tua pagina è stata aggiornata.',
+	'created' => 'La tua pagina è stata creata.',
+	'deleted' => 'La tua pagina è stata eliminata.'
+
+);
---
+++ anchor/language/it_IT/posts.php
@@ -0,0 +1,47 @@
+<?php
+
+return array(
+
+	'posts' => 'Articoli',
+
+	'create_post' => 'Crea un nuovo Articolo',
+	'noposts_desc' => 'Non hai nessun Articolo!',
+
+	// form fields
+	'title' => 'Titolo Articolo',
+	'title_explain' => '',
+	'title_missing' => 'Perfavore inserisci un titolo',
+
+	'content' => 'Contenuto del tuo articolo',
+	'content_explain' => 'Scrivi...',
+
+	'slug' => 'Slug',
+	'slug_explain' => 'URI Slug per identificare la tua pagina, dovrebbe contenere solo caratteri ASCII',
+	'slug_missing' => 'Perfavore inserisci un URI Slug, dovrebbe contenere solo caratteri ASCII',
+	'slug_duplicate' => 'Questo Slug già esiste',
+	'slug_invalid' => 'Lo Slug deve contenere delle lettere',
+
+	'description' => 'Descrizione',
+	'description_explain' => '',
+
+	'status' => 'Stato',
+	'status_explain' => '',
+
+	'category' => 'Categoria',
+	'category_explain' => '',
+
+	'allow_comments' => 'Permetti Commenti',
+	'allow_comments_explain' => '',
+
+	'custom_css' => 'CSS personalizzato',
+	'custom_css_explain' => '',
+
+	'custom_js' => 'JS personalizzato',
+	'custom_js_explain' => '',
+
+	// messages
+	'updated' => 'Il tuo articolo è stato aggiornato',
+	'created' => 'Il tuo articolo è stato creato',
+	'deleted' => 'Il tuo articolo è stato eliminato'
+
+);
