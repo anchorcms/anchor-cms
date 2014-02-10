a3788eb099cedcc57113cee8031ac6409a300025
Kieron <k.wilson@nigelfrank.com>
2014-02-10 11:49:36 +0000

    fix publishing assets

--- anchor/publish.php
+++ anchor/publish.php
@@ -2,18 +2,21 @@
 
 require __DIR__ . '/autoload.php';
 
-/**
- * Publish Anchor assets to the public folder
- */
-$public = __DIR__ . '/../public/vendor/anchorcms/anchor-core';
-
-if( ! is_dir($public)) mkdir($public, 0701, true);
-
-$core = realpath(__DIR__ . '/../../anchor-core/public');
-
 $l = new lessc;
 $l->setFormatter('compressed');
 
 $assets = new Anchor\Services\Assets($l);
-$assets->copy($core, $public);
-$assets->compile($public);
+
+$packages = array('anchorcms/anchor-core');
+
+foreach($packages as $package) {
+	$src = __DIR__ . '/../vendor/'.$package.'/public';
+	$dest = __DIR__ . '/../public/vendor/'.$package;
+
+	if( ! is_dir($dest)) {
+		mkdir($dest, 701, true);
+	}
+
+	$assets->copy($src, $dest);
+	$assets->compile($dest);
+}
