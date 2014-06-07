81b803854e0efc9af5c0324ac16a1a1ff7fd8a97
hafezdivandari <hafezdivandari@gmail.com>
2014-06-07 18:45:52 +0430

    Create metadata.php

---
+++ anchor/language/fa_IR/metadata.php
@@ -0,0 +1,45 @@
+<?php
+
+return array(
+
+	'metadata' => 'فرادادۀ سایت',
+	'metadata_desc' => 'داده‌های سایت خود را مدیریت کنید',
+
+	'comment_settings' => 'نظرات',
+	'theme_settings' => 'ظاهر',
+
+	// form fields
+	'sitename' => 'نام سایت',
+	'sitename_explain' => '',
+	'sitename_missing' => 'سایت شما به نام نیاز دارد!',
+
+	'sitedescription' => 'توصیف سایت',
+	'sitedescription_explain' => '',
+	'sitedescription_missing' => 'سایت شما به توصیف نیاز دارد!',
+
+	'homepage' => 'صفحۀ اصلی',
+	'homepage_explain' => '',
+
+	'postspage' => 'صفحۀ پست‌ها',
+	'postspage_explain' => '',
+
+	'posts_per_page' => 'پست در هر صفحه',
+	'posts_per_page_explain' => '',
+
+	'auto_publish_comments' => 'تائید خودکار نظرات',
+	'auto_publish_comments_explain' => '',
+
+	'comment_notifications' => 'آگاه‌سازی از طریق رایانامه برای نظرات جدید',
+	'comment_notifications_explain' => '',
+
+	'comment_moderation_keys' => 'کلیدواژه‌های هرز',
+	'comment_moderation_keys_explain' => 'فهرستی از کلیدواژه‌های غیر مجاز که بوسیلۀ کاما از هم جدا شده‌اند.
+		نظرات به صورت خودکار هرز تلقی می‌شوند.',
+
+	'current_theme' => 'پوستۀ کنونی',
+	'current_theme_explain' => '',
+
+	// messages
+	'updated' => 'فراداده به‌روز شد',
+
+);
