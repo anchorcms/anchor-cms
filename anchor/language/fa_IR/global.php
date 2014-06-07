820d6c6ab079e4a9e61bb4995e897666347dc096
hafezdivandari <hafezdivandari@gmail.com>
2014-06-07 18:46:42 +0430

    Create global.php

---
+++ anchor/language/fa_IR/global.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * Global is to replace common.php
+ *
+ * This global file will provide the majority of the translations in anchor
+ * to remove duplicate translations in section specific language files.
+ */
+return array(
+
+	// words
+	'save' => 'ذخیره',
+	'delete' => 'حذف',
+	'update' => 'به‌روزرسانی',
+	'edit' => 'ویرایش',
+	'editing' => 'ویرایش',
+	'create' => 'ایجاد',
+	'created' => 'ایجاد شده',
+	'submit' => 'ارسال',
+	'close' => 'بستن',
+	'status' => 'وضعیت',
+	'manage' => 'مدیریت',
+	'reset' => 'بازنشانی',
+	'all' => 'همه',
+
+	// pagination
+	'next' => 'بعدی',
+	'previous' => 'قبلی',
+	'first' => 'اولی',
+	'last' => 'آخری',
+
+	// statuses
+	'draft' => 'پیش نویس',
+	'archived' => 'بایگانی شده',
+	'published' => 'منتشر شده',
+	'pending' => 'منتظر',
+	'approved' => 'تائید شده',
+	'spam' => 'هرز',
+
+	'inactive' => 'غیرفعال',
+	'active' => 'فعال',
+
+	// roles
+	'administrator' => 'مدیر',
+	'editor' => 'ویرایشگر',
+	'user' => 'کاربر',
+
+	'log_in' => 'ورود',
+	'login' => 'ورود',
+	'log_out' => 'خروج',
+	'logout' => 'خروج',
+
+	// pharses
+	'visit_your_site' => 'از سایت‌تان بازدید کنید',
+	'powered_by_anchor' => 'قدرت گرفته از Anchor، نسخۀ %s',
+	'make_blogging_beautiful' => 'بلاگ‌نویسی را زیبا کنید.',
+
+	// intro
+	'welcome_to_anchor' => 'به Anchor خوش‌آمدید',
+	'welcome_to_anchor_lets_go' => 'به Anchor خوش‌آمدید. بزن بریم.',
+	'run_the_installer' => 'اجرای برنامۀ نصب',
+
+	// upgrade
+	'upgrade' => 'به‌سازی',
+	'good_news' => 'خبرهای خوب!',
+	'new_version_available' => 'نسخۀ جدید Anchor در دسترس است!',
+	'download_now' => 'الآن دانلود کن',
+	'upgrade_later' => 'بعداً به‌روزرسانی کن',
+
+	// debug profiler
+	'profile' => 'حساب شخصی',
+	'profile_memory_usage' => 'کل حافظۀ مصرفی',
+
+	// messages
+	'confirm_delete' => 'آیا مطمئن هستید که می‌خواهید حذف کنید؟ این عمل برگشت‌ناپذیر است!'
+
+);
