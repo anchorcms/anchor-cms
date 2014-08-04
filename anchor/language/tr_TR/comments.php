d88bb79a0cefc041434ef5febd5de91169572f8d
nblackburn <nblackburn@fastmail.fm>
2014-08-04 19:56:30 +0000

    Added tr_TR language

---
+++ anchor/language/tr_TR/comments.php
@@ -0,0 +1,35 @@
+<?php
+
+return array(
+
+	'comments' => 'Yorumlar',
+	'nocomments_desc' => 'Hen�z yorum yok.',
+	'editing_comment' => 'Yorum d�zenleme',
+	'view_comment' => 'Yorum g�r�nt�le',
+
+	// form fields
+	'name' => 'Name',
+	'name_explain' => 'Yazar ad�',
+	'name_missing' => 'L�tfen isim girin',
+
+	'email' => 'Eposta adresi',
+	'email_explain' => 'Yazar epostas�',
+	'email_missing' => 'Ge�erli bir eposta adresi girin', // frontend message (appears on your site!)
+
+	'text' => 'Yorum',
+	'text_explain' => '',
+	'text_missing' => 'Yorum yaz�s� girin', // frontend message (appears on your site!)
+
+	'status' => 'Durum',
+	'status_explain' => '',
+
+	// messages
+	'created' => 'Yorumunuz eklendi', // frontend message (appears on your site!)
+	'updated' => 'Yorumunuz g�ncellendi',
+	'deleted' => 'Yorumunuz silindi',
+
+	// email notification
+	'notify_subject' => 'Yeni yorum eklendi',
+	'nofity_heading' => 'Sitenize yeni bir yorum g�nderildi.'
+
+);
---
+++ anchor/language/tr_TR/pages.php
@@ -0,0 +1,45 @@
+<?php
+
+return array(
+
+	'pages' => 'Sayfalar',
+
+	'create_page' => 'Yeni sayfa oluştur',
+	'nopages_desc' => 'Herhangi bir sayfa yok.',
+	'redirect' => 'Yönlendirme',
+
+	// form fields
+	'redirect_url' => 'Yönlendirilecek Url',
+	'redirect_missing' => 'Geçerli bir url girin',
+
+	'title' => 'Sayfa başlığı',
+	'title_explain' => '',
+	'title_missing' => 'Sayfa başlığı girin',
+
+	'content' => 'İçerik',
+	'content_explain' => 'Sayfa içeriğiniz.',
+
+	'show_in_menu' => 'Menüyü Göster',
+	'show_in_menu_explain' => '',
+
+	'name' => 'İsim',
+	'name_explain' => '',
+
+	'slug' => 'Sabit Değer',
+	'slug_explain' => 'Sayfanızı tanımlamak için sabit bir URL tanımlayın. Sadece ascii karakterler içerebilir',
+	'slug_missing' => 'Lütfen sabit bir URL girin. Sabit değer sadece ascii karakter içerebilir.',
+	'slug_duplicate' => 'Sabit değer zaten var',
+	'slug_invalid' => 'Sabit değer harf içermelidir',
+
+	'status' => 'Durum',
+	'status_explain' => '',
+
+	'parent' => 'Ana',
+	'parent_explain' => '',
+
+	// messages
+	'updated' => 'Sayfanız güncellendi.',
+	'created' => 'Sayfanız güncellendi.',
+	'deleted' => 'Sayfanız güncellendi.'
+
+);
