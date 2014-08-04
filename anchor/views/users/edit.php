673d11bceb179d6c74b4041a7d602d074cc77d57
nblackburn <nblackburn@fastmail.fm>
2014-08-04 20:29:20 +0000

    Prevented users from removing themselve, this prevents people from locking themselves out of the system

--- anchor/views/users/edit.php
+++ anchor/views/users/edit.php
@@ -13,54 +13,57 @@
 
 		<fieldset class="half split">
 			<p>
-				<label for="label-real_name"><?php echo __('users.real_name'); ?>:</label>
-				<?php echo Form::text('real_name', Input::previous('real_name', $user->real_name), array('id' => 'label-real_name')); ?>
+				<label><?php echo __('users.real_name'); ?>:</label>
+				<?php echo Form::text('real_name', Input::previous('real_name', $user->real_name)); ?>
 				<em><?php echo __('users.real_name_explain'); ?></em>
 			</p>
 			<p>
-				<label for="label-bio"><?php echo __('users.bio'); ?>:</label>
-				<?php echo Form::textarea('bio', Input::previous('bio', $user->bio), array('cols' => 20, 'id' => 'label-bio')); ?>
+				<label><?php echo __('users.bio'); ?>:</label>
+				<?php echo Form::textarea('bio', Input::previous('bio', $user->bio), array('cols' => 20)); ?>
 				<em><?php echo __('users.bio_explain'); ?></em>
 			</p>
 			<p>
-				<label for="label-status"><?php echo __('users.status'); ?>:</label>
-				<?php echo Form::select('status', $statuses, Input::previous('status', $user->status), array('id' => 'label-status')); ?>
+				<label><?php echo __('users.status'); ?>:</label>
+				<?php echo Form::select('status', $statuses, Input::previous('status', $user->status)); ?>
 				<em><?php echo __('users.status_explain'); ?></em>
 			</p>
 			<p>
-				<label for="label-role"><?php echo __('users.role'); ?>:</label>
-				<?php echo Form::select('role', $roles, Input::previous('role', $user->role), array('id' => 'label-role')); ?>
+				<label><?php echo __('users.role'); ?>:</label>
+				<?php echo Form::select('role', $roles, Input::previous('role', $user->role)); ?>
 				<em><?php echo __('users.role_explain'); ?></em>
 			</p>
 		</fieldset>
 
 		<fieldset class="half split">
 			<p>
-				<label for="label-username"><?php echo __('users.username'); ?>:</label>
-				<?php echo Form::text('username', Input::previous('username', $user->username), array('id' => 'label-username')); ?>
+				<label><?php echo __('users.username'); ?>:</label>
+				<?php echo Form::text('username', Input::previous('username', $user->username)); ?>
 				<em><?php echo __('users.role_explain'); ?></em>
 			</p>
 			<p>
-				<label for="label-password"><?php echo __('users.password'); ?>:</label>
-				<?php echo Form::password('password', array('id' => 'label-password')); ?>
+				<label><?php echo __('users.password'); ?>:</label>
+				<?php echo Form::password('password'); ?>
 				<em><?php echo __('users.password_explain'); ?></em>
 			</p>
 			<p>
-				<label for="label-email"><?php echo __('users.email'); ?>:</label>
-				<?php echo Form::text('email', Input::previous('email', $user->email), array('id' => 'label-email')); ?>
+				<label><?php echo __('users.email'); ?>:</label>
+				<?php echo Form::text('email', Input::previous('email', $user->email)); ?>
 				<em><?php echo __('users.email_explain'); ?></em>
 			</p>
 		</fieldset>
 
 		<aside class="buttons">
-			<?php echo Form::button(__('global.update'), array(
+			
+            <?php echo Form::button(__('global.update'), array(
 				'class' => 'btn',
 				'type' => 'submit'
 			)); ?>
 
-			<?php echo Html::link('admin/users/delete/' . $user->id, __('global.delete'), array('class' => 'btn delete red')); ?>
-		</aside>
-	</form>
+			<?php if (Auth::user()->id !== $user->id) echo Html::link('admin/users/delete/' . $user->id, __('global.delete'), array('class' => 'btn delete red')); ?>
+		
+        </aside>
+	
+    </form>
 
 </section>
 
