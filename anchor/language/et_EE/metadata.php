65a2ae95d46490520a1fbbdacae071e68b7a1b33
nblackburn <nblackburn@fastmail.fm>
2014-08-04 19:52:49 +0000

    Added et_EE language

---
+++ anchor/language/et_EE/metadata.php
@@ -0,0 +1,29 @@
+<?php
+
+return array(
+
+	'metadata' => 'Lehe seaded',
+
+	'sitename' => 'Lehe nimi',
+	'sitename_explain' => 'Su lehe pealkiri',
+	'sitedescription' => 'Lehe kirjeldus',
+	'sitedescription_explain' => 'Lühike paragrahv mis selgitab Su lehte.',
+	'homepage' => 'Koduleht',
+	'homepage_explain' => 'Su praegune koduleht.',
+	'postspage' => 'Postituste leht',
+	'postspage_explain' => 'Leht mis kuvab Su postitusi.',
+	'posts_per_page' => 'Postitusi lehe peal',
+	'posts_per_page_explain' => 'Postituste arv mida kuvada postituste lehel.',
+	'current_theme' => 'Praegune teema',
+	'current_theme_explain' => 'Su praegune teema.',
+	'auto_publish_comments' => 'Automaatselt avalikusta kommentaarid',
+	'twitter' => 'Twitter',
+	'twitter_explain' => 'Su twitter konto. Kuvatakse @<span id="output"></span> kujul.',
+	'save' => 'Salvesta muudatused',
+
+	'missing_sitename' => 'Sul puudub lehe nimi',
+	'missing_sitedescription' => 'Sul puudub lehe kirjeldus',
+	'missing_theme' => 'Sul on vaja teemat',
+	'meta_success_updated' => 'Su lehe seaded on uuendatud'
+
+);
