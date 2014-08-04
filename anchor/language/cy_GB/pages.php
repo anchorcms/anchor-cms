71f7d6dd3cca7fb76f01f39694ca5d71a025dcc9
nblackburn <nblackburn@fastmail.fm>
2014-08-04 19:50:03 +0000

    Added cy_GB language

---
+++ anchor/language/cy_GB/pages.php
@@ -0,0 +1,41 @@
+<?php
+
+return array(
+
+	'pages' => 'Dudalennau',
+	'create_page' => 'Creu tudalen',
+	'no_pages' => 'Dim tudalennau. Eisiau i greu un?',
+	'add_page' => 'Creu tudalen',
+	'editing' => 'Golygu',
+	'editing_explain' => 'Hypergysylltiadau defnyddiol.',
+	'view_page' => 'Rhagolwg y dudalen hon',
+
+	'name' => 'Enw',
+		'name_explain' => 'Yr enw dudalen. Mae hyn yn weladwy.',
+	'title' => 'Teitl',
+		'title_explain' => 'Teitl y dudalen. Mae hyn yn anweledig.',
+	'slug' => 'Gwlithod',
+		'slug_explain' => 'Y wlithen swydd (<code>' . $_SERVER['HTTP_HOST'] . '/<span id="output">slug</span></code>).',
+	'content' => 'Cynnwys',
+		'content_explain' => 'Cynnwys tudalen. Defnyddio HTML.',
+	'status' => 'Status',
+		'status_explain' => 'Statws eich tudalen',
+
+	'draft' => 'Drafft',
+	'archived' => 'Archif',
+	'published' => 'Gyhoeddi',
+
+	'create' => 'Create',
+	'delete' => 'Dileu',
+	'save' => 'Cadw\'r newidiadau',
+	'return_pages' => 'Dychwelyd i\'r tudalennau',
+
+	'missing_name' => 'Os gwelwch yn dda rhowch enw',
+	'missing_title' => 'Rhowch deitl',
+	'duplicate_slug' => 'Hyn gwlithod yn bodoli eisoes.',
+	'page_success_created' => 'Y dudalen hon wedi cael ei ychwanegu',
+	'page_success_updated' => 'Y dudalen hon wedi bod yn ei olygu',
+	'page_success_delete' => 'Y dudalen hon wedi cael ei ddileu',
+	'page_error_delete' => 'Nad ydych yn gallu dileu y dudalen hon'
+
+);
