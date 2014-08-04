9f062f43cb3ff01211dade071d4b17d89173396d
nblackburn <nblackburn@fastmail.fm>
2014-08-04 19:44:12 +0000

    Added nl_NL language

---
+++ anchor/language/nl_NL/categories.php
@@ -0,0 +1,28 @@
+<?php
+
+return array(
+
+	'category' => 'Categorie',
+	'categories' => 'Categorieën',
+
+	'create_category' => 'Maak een nieuwe categorie',
+	'edit_category' => 'Bewerk &ldquo;%s&rdquo;',
+
+	// form fields - formulier velden
+	'title' => 'Titel',
+	'title_explain' => 'Titel van jouw categorie.',
+	'title_missing' => 'Vul een titel in',
+
+	'slug' => 'Slug',
+	'slug_explain' => 'De slug voor je categorie.',
+
+	'description' => 'Beschrijvong',
+	'description_explain' => 'Waar jouw categorie over gaat.',
+
+	// messages - berichten
+	'created' => 'Je nieuwe categorie is toegevoegd.',
+	'updated' => 'Je categorie is bijgewerkt.',
+	'deleted' => 'Je categorie is verwijderd.',
+	'delete_error' => 'Je hebt minimaal &#233;&#233;n categorie nodig.',
+
+);
