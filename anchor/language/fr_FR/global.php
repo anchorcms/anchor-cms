002372e32968d4b50b48c80e764e63d1aebdbf06
nblackburn <nblackburn@fastmail.fm>
2014-08-04 19:54:43 +0000

    Added fr_FR language

---
+++ anchor/language/fr_FR/global.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * Global is to replace common.php
+ *
+ * This global file will provide the majority of the translations in anchor
+ * to remove duplicate translations in section specific language files.
+ */
+return array(
+
+	// words
+	'save' => 'Sauvegarder',
+	'delete' => 'Supprimer',
+	'update' => 'Mettre à jour',
+	'edit' => 'Editer',
+	'editing' => 'Edition',
+	'create' => 'Créer',
+	'created' => 'Créé',
+	'submit' => 'Envoyer',
+	'close' => 'Fermer',
+	'status' => 'Status',
+	'manage' => 'Gérer',
+	'reset' => 'Réinitialiser',
+	'all' => 'Tous',
+
+	// pagination
+	'next' => 'Suivant',
+	'previous' => 'Avant',
+	'first' => 'Premier',
+	'last' => 'Dernier',
+
+	// statuses
+	'draft' => 'Brouillon',
+	'archived' => 'Archivé',
+	'published' => 'Publié',
+	'pending' => 'En attente',
+	'approved' => 'Approuvé',
+	'spam' => 'Spam',
+
+	'inactive' => 'Inactif',
+	'active' => 'Actif',
+
+	// roles
+	'administrator' => 'Admin',
+	'editor' => 'Editeur',
+	'user' => 'Utilisateur',
+
+	'log_in' => 'Connecté',
+	'login' => 'Se connecter',
+	'log_out' => 'Se déconnecter',
+	'logout' => 'Déconnexion',
+
+	// pharses
+	'visit_your_site' => 'Voir',
+	'powered_by_anchor' => 'Propulsé par Anchor, version %s',
+	'make_blogging_beautiful' => 'Make blogging beautiful.',
+
+	// intro
+	'welcome_to_anchor' => 'Bienvenue dans Anchor',
+	'welcome_to_anchor_lets_go' => 'Bienvenue dans Anchor. C\'est parti !',
+	'run_the_installer' => 'Lancer l\'installation',
+
+	// upgrade
+	'upgrade' => 'Mettre à niveau',
+	'good_news' => 'Bonne nouvelle !',
+	'new_version_available' => 'Il existe une nouvelle version de anchor disponible.',
+	'download_now' => 'Télécharger maintenant',
+	'upgrade_later' => 'Mettre à miveau plus tard',
+
+	// debug profiler
+	'profile' => 'Profil',
+	'profile_memory_usage' => 'Utilisation totale de la mémoire',
+
+	// messages
+	'confirm_delete' => 'Êtes-vous sûr(e) de vouloir supprimer ? Aucune restauration possible !'
+
+);
---
+++ anchor/language/fr_FR/pages.php
@@ -0,0 +1,45 @@
+<?php
+
+return array(
+
+	'pages' => 'Pages',
+
+	'create_page' => 'Créer une page',
+	'nopages_desc' => 'Aucune page pour l\'instant.',
+	'redirect' => 'Rediriger',
+
+	// form fields
+	'redirect_url' => 'URL de redirection',
+	'redirect_missing' => 'Veuillez renseigner une URL valide',
+
+	'title' => 'Titre de la page',
+	'title_explain' => '',
+	'title_missing' => 'Veuillez renseigner un titre',
+
+	'content' => 'Contenu',
+	'content_explain' => 'Le contenu de la page. Utilisez Markdown.',
+
+	'show_in_menu' => 'Visible dans le menu ',
+	'show_in_menu_explain' => '',
+
+	'name' => 'Nom ',
+	'name_explain' => '',
+
+	'slug' => 'Alias ',
+	'slug_explain' => 'Partie d\'URL propre à la page, doit contenir des caractères ascii uniquement.',
+	'slug_missing' => 'Veuillez renseigner un Alias, doit contenir des caractères ascii uniquement',
+	'slug_duplicate' => 'Cet Alias existe déjà',
+	'slug_invalid' => 'L\'Alias doit contenir des lettres uniquement',
+
+	'status' => 'Statut ',
+	'status_explain' => '',
+
+	'parent' => 'Parent ',
+	'parent_explain' => '',
+
+	// messages
+	'updated' => 'La page a été mise à jour.',
+	'created' => 'La page a été créée.',
+	'deleted' => 'La page a été supprimée.'
+
+);
