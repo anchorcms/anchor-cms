e58714a28adf507921fbdd1d807a43a9c53d4de7
nblackburn <nblackburn@fastmail.fm>
2014-08-04 19:55:21 +0000

    Added pl_PL language

---
+++ anchor/language/pl_PL/metadata.php
@@ -0,0 +1,45 @@
+<?php
+
+return array(
+
+	'metadata' => 'Metadane strony',
+	'metadata_desc' => 'Zarządzaj danymi strony',
+
+	'comment_settings' => 'Komentarze',
+	'theme_settings' => 'Wygląd',
+
+	// form fields
+	'sitename' => 'Nazwa strony',
+	'sitename_explain' => '',
+	'sitename_missing' => 'Twoja strona potrzebuje nazwy!',
+
+	'sitedescription' => 'Opis strony',
+	'sitedescription_explain' => '',
+	'sitedescription_missing' => 'Twoja strona potrzebuje opisu!',
+
+	'homepage' => 'Strona główna',
+	'homepage_explain' => '',
+
+	'postspage' => 'Strona z wpisami',
+	'postspage_explain' => '',
+
+	'posts_per_page' => 'Postów na stronę',
+	'posts_per_page_explain' => '',
+
+	'auto_publish_comments' => 'Auto-komentarze',
+	'auto_publish_comments_explain' => 'Jeśli zaznaczone, komentarze będą domyślnie aktywne.',
+
+	'comment_notifications' => 'Powiadomienie e-mail',
+	'comment_notifications_explain' => 'Powiadomienie e-mail o nowych komentarzach',
+
+	'comment_moderation_keys' => 'Spam',
+	'comment_moderation_keys_explain' => 'Oddzielone przecinkami słowa, które trafią na czarna listę.
+		Komentarze z tymi słowami zostaną oznaczone jako spam.',
+
+	'current_theme' => 'Motyw',
+	'current_theme_explain' => '',
+
+	// messages
+	'updated' => 'Metadane zostały zaktualizowane',
+
+);
---
+++ anchor/language/pl_PL/plugins.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+
+	'plugins' => 'Rozszerzenia',
+	'plugins_desc' => 'Zarządzaj rozszerzeniami',
+	'noplugins_desc' => 'Brak rozszerzeń',
+
+	// messages
+	'installed' => 'Wtyczka %s została zainstalowana',
+	'uninstalled' => 'Wtyczka %s zostałą odinstalowana'
+);
