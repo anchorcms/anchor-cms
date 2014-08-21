bc8700da38eac758a752ad6d702db1e323a7a813
MichalP <mpp@linuxmail.org>
2014-08-21 10:24:49 +0200

    Polish language translation
    Polish language translation pack added

---
+++ anchor/language/pl_PL/categories.php
@@ -0,0 +1,28 @@
+<?php
+
+return array(
+
+	'category' => 'Kategoria',
+	'categories' => 'Kategorie',
+
+	'create_category' => 'Utwórz kategorię',
+	'edit_category' => 'Edytujesz &ldquo;%s&rdquo;',
+
+	// form fields
+	'title' => 'Tytuł',
+	'title_explain' => 'Tytuł kategorii.',
+	'title_missing' => 'Proszę wpisać tytuł kategorii.',
+
+	'slug' => 'Skrócony adres',
+	'slug_explain' => 'Skrócony adres dla Twojej kategorii.',
+
+	'description' => 'Opis',
+	'description_explain' => 'Jaka jest tematyka tej kategorii?',
+
+	// messages
+	'created' => 'Utworzono nową kategorię.',
+	'updated' => 'Kategoria została zaktualizowana.',
+	'deleted' => 'Kategoria została usunięta.',
+	'delete_error' => 'Musisz mieć conajmniej jedną kategorię.',
+
+);
---
+++ anchor/language/pl_PL/pages.php
@@ -0,0 +1,45 @@
+<?php
+
+return array(
+
+	'pages' => 'Strony',
+
+	'create_page' => 'Stwórz nową stronę',
+	'nopages_desc' => 'Nie masz żadnych stron.',
+	'redirect' => 'Przekierowanie',
+
+	// form fields
+	'redirect_url' => 'Url przekierowania',
+	'redirect_missing' => 'Wpisz prawidłowy url',
+
+	'title' => 'Tytuł strony',
+	'title_explain' => '',
+	'title_missing' => 'Prosze wpisać tytuł strony',
+
+	'content' => 'Treść',
+	'content_explain' => 'Treść twojej strony. Użyj formatowania Markdown',
+
+	'show_in_menu' => 'Pokaż w menu',
+	'show_in_menu_explain' => '',
+
+	'name' => 'Nazwa',
+	'name_explain' => '',
+
+	'slug' => 'Skrócony adres',
+	'slug_explain' => 'Skrócony adres uri identyfikujący Twoją stronę, powinien zawierać tylko znaki ascii',
+	'slug_missing' => 'Prosze wpisać skrócony adres uri, może on zawierać tylko znaki ascii',
+	'slug_duplicate' => 'Skrócony adres jest już zajęty',
+	'slug_invalid' => 'Skrócony adres musi zawierać litery',
+
+	'status' => 'Status',
+	'status_explain' => '',
+
+	'parent' => 'Rodzic',
+	'parent_explain' => '',
+
+	// messages
+	'updated' => 'Twoja strona została zaktualizowana.',
+	'created' => 'Twoja strona została utworzona.',
+	'deleted' => 'Twoja strona została usunięta.'
+
+);
