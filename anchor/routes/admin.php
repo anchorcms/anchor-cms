d362971ab51d274f8c51156fdaa21d54adcfc321
Yukari Mustang <yuka.mustang@gmail.com>
2014-09-13 11:52:17 -0300

    Prevent brute force attack

--- anchor/routes/admin.php
+++ anchor/routes/admin.php
@@ -33,142 +33,152 @@ Route::get('admin', function() {
 	Log in
 */
 Route::get('admin/login', function() {
-	$vars['messages'] = Notify::read();
-	$vars['token'] = Csrf::token();
+    $canAttempt = Auth::canAttempt();
+    if ( ! $canAttempt) {
+        Notify::error(__('users.login_attempts_reached', Auth::getMaxAttemptsTimeout()));
+    }
+    $vars['messages'] = Notify::read();
+    $vars['token'] = Csrf::token();
+    $vars['canAttempt'] = $canAttempt;
 
-	return View::create('users/login', $vars)
-		->partial('header', 'partials/header')
-		->partial('footer', 'partials/footer');
+    return View::create('users/login', $vars)
+            ->partial('header', 'partials/header')
+            ->partial('footer', 'partials/footer');
 });
 
 Route::post('admin/login', array('before' => 'csrf', 'main' => function() {
-	$attempt = Auth::attempt(Input::get('user'), Input::get('pass'));
+    if (Auth::canAttempt()) {
+        $attempt = Auth::attempt(Input::get('user'), Input::get('pass'));
+        if( ! $attempt) {
+            Notify::error(__('users.login_error'));
+            return Response::redirect('admin/login');
+        } else {
+            // check for updates
+            Update::version();
 
-	if( ! $attempt) {
-		Notify::error(__('users.login_error'));
+            if(version_compare(Config::get('meta.update_version'), VERSION, '>')) {
+                return Response::redirect('admin/upgrade');
+            }
 
-		return Response::redirect('admin/login');
-	}
+            Auth::resetAttempts();
 
-	// check for updates
-	Update::version();
-
-	if(version_compare(Config::get('meta.update_version'), VERSION, '>')) {
-		return Response::redirect('admin/upgrade');
-	}
-
-	return Response::redirect('admin/posts');
+            return Response::redirect('admin/posts');
+        }
+    } else {
+        Notify::error(__('users.login_attempts_reached', Auth::getMaxAttemptsTimeout()));
+        return Response::redirect('admin/login');
+    }
 }));
 
 /*
 	Log out
 */
 Route::get('admin/logout', function() {
-	Auth::logout();
-	Notify::notice(__('users.logout_notice'));
-	return Response::redirect('admin/login');
+    Auth::logout();
+    Notify::notice(__('users.logout_notice'));
+    return Response::redirect('admin/login');
 });
 
 /*
 	Amnesia
 */
 Route::get('admin/amnesia', array('before' => 'guest', 'main' => function() {
-	$vars['messages'] = Notify::read();
-	$vars['token'] = Csrf::token();
+            $vars['messages'] = Notify::read();
+            $vars['token'] = Csrf::token();
 
-	return View::create('users/amnesia', $vars)
-		->partial('header', 'partials/header')
-		->partial('footer', 'partials/footer');
-}));
+            return View::create('users/amnesia', $vars)
+                    ->partial('header', 'partials/header')
+                    ->partial('footer', 'partials/footer');
+        }));
 
 Route::post('admin/amnesia', array('before' => 'csrf', 'main' => function() {
-	$email = Input::get('email');
+            $email = Input::get('email');
 
-	$validator = new Validator(array('email' => $email));
-	$query = User::where('email', '=', $email);
+            $validator = new Validator(array('email' => $email));
+            $query = User::where('email', '=', $email);
 
-	$validator->add('valid', function($email) use($query) {
-		return $query->count();
-	});
+            $validator->add('valid', function($email) use($query) {
+                return $query->count();
+            });
 
-	$validator->check('email')
-		->is_email(__('users.email_missing'))
-		->is_valid(__('users.email_not_found'));
+            $validator->check('email')
+                    ->is_email(__('users.email_missing'))
+                    ->is_valid(__('users.email_not_found'));
 
-	if($errors = $validator->errors()) {
-		Input::flash();
+            if($errors = $validator->errors()) {
+                Input::flash();
 
-		Notify::error($errors);
+                Notify::error($errors);
 
-		return Response::redirect('admin/amnesia');
-	}
+                return Response::redirect('admin/amnesia');
+            }
 
-	$user = $query->fetch();
-	Session::put('user', $user->id);
+            $user = $query->fetch();
+            Session::put('user', $user->id);
 
-	$token = noise(8);
-	Session::put('token', $token);
+            $token = noise(8);
+            Session::put('token', $token);
 
-	$uri = Uri::full('admin/reset/' . $token);
-	$subject = __('users.recovery_subject');
-	$msg = __('users.recovery_message', $uri);
+            $uri = Uri::full('admin/reset/' . $token);
+            $subject = __('users.recovery_subject');
+            $msg = __('users.recovery_message', $uri);
 
-	mail($user->email, $subject, $msg);
+            mail($user->email, $subject, $msg);
 
-	Notify::success(__('users.recovery_sent'));
+            Notify::success(__('users.recovery_sent'));
 
-	return Response::redirect('admin/login');
-}));
+            return Response::redirect('admin/login');
+        }));
 
 /*
 	Reset password
 */
 Route::get('admin/reset/(:any)', array('before' => 'guest', 'main' => function($key) {
-	$vars['messages'] = Notify::read();
-	$vars['token'] = Csrf::token();
-	$vars['key'] = ($token = Session::get('token'));
+            $vars['messages'] = Notify::read();
+            $vars['token'] = Csrf::token();
+            $vars['key'] = ($token = Session::get('token'));
 
-	if($token != $key) {
-		Notify::error(__('users.recovery_expired'));
+            if($token != $key) {
+                Notify::error(__('users.recovery_expired'));
 
-		return Response::redirect('admin/login');
-	}
+                return Response::redirect('admin/login');
+            }
 
-	return View::create('users/reset', $vars)
-		->partial('header', 'partials/header')
-		->partial('footer', 'partials/footer');
-}));
+            return View::create('users/reset', $vars)
+                    ->partial('header', 'partials/header')
+                    ->partial('footer', 'partials/footer');
+        }));
 
 Route::post('admin/reset/(:any)', array('before' => 'csrf', 'main' => function($key) {
-	$password = Input::get('pass');
-	$token = Session::get('token');
-	$user = Session::get('user');
+            $password = Input::get('pass');
+            $token = Session::get('token');
+            $user = Session::get('user');
 
-	if($token != $key) {
-		Notify::error(__('users.recovery_expired'));
+            if($token != $key) {
+                Notify::error(__('users.recovery_expired'));
 
-		return Response::redirect('admin/login');
-	}
+                return Response::redirect('admin/login');
+            }
 
-	$validator = new Validator(array('password' => $password));
+            $validator = new Validator(array('password' => $password));
 
-	$validator->check('password')
-		->is_max(6, __('users.password_too_short', 6));
+            $validator->check('password')
+                    ->is_max(6, __('users.password_too_short', 6));
 
-	if($errors = $validator->errors()) {
-		Input::flash();
+            if($errors = $validator->errors()) {
+                Input::flash();
 
-		Notify::error($errors);
+                Notify::error($errors);
 
-		return Response::redirect('admin/reset/' . $key);
-	}
+                return Response::redirect('admin/reset/' . $key);
+            }
 
-	User::update($user, array('password' => Hash::make($password)));
+            User::update($user, array('password' => Hash::make($password)));
 
-	Session::erase('user');
-	Session::erase('token');
+            Session::erase('user');
+            Session::erase('token');
 
-	Notify::success(__('users.password_reset'));
+            Notify::success(__('users.password_reset'));
 
 	return Response::redirect('admin/login');
 }));
