303d036c969f6a9a6e7bf4b964068ada127e7cbc
CraigChilds94 <childscraig17@gmail.com>
2014-09-25 14:04:31 +0100

    Fix #719 now doesn't remove capital letters

--- anchor/helpers.php
+++ anchor/helpers.php
@@ -15,14 +15,14 @@ function is_installed() {
 }
 
 function slug($string, $separator = '-') {
-	$accents_regex = '~&([a-z]{1,2})(?:acute|cedil|circ|grave|lig|orn|ring|slash|th|tilde|uml);~i';
+	$accents_regex = '~&([a-zA-Z]{1,2})(?:acute|cedil|circ|grave|lig|orn|ring|slash|th|tilde|uml);~i';
 	$special_cases = array(
 		'&' => 'and'
 	);
 	$string = mb_strtolower(trim($string) , 'UTF-8');
 	$string = str_replace(array_keys($special_cases) , array_values($special_cases) , $string);
 	$string = preg_replace($accents_regex, '$1', htmlentities($string, ENT_QUOTES, 'UTF-8'));
-	$string = preg_replace("/[^a-z0-9]/u", "$separator", $string);
+	$string = preg_replace("/[^a-zA-Z0-9]/u", "$separator", $string);
 	$string = preg_replace("/[$separator]+/u", "$separator", $string);
 	$string = trim($string, '-');
 	return $string;
