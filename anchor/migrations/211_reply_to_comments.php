574fd5f05a73a643104c0e076940818190ebb3db
Devon Hazelett <xdev@devxdev.com>
2014-10-19 19:13:37 -0700

    Allow single level replies to comments

---
+++ anchor/migrations/211_reply_to_comments.php
@@ -0,0 +1,19 @@
+<?php
+
+class Migration_reply_to_comments extends Migration {
+
+	public function up() {
+		$table = Base::table('comments');
+
+		DB::ask("ALTER TABLE `{$table}` ADD `reply_to` INT(6) UNSIGNED NOT NULL DEFAULT '0'");
+
+	}
+
+	public function down() {
+		$table = Base::table('comments');
+
+		DB::ask("ALTER TABLE `{$table}` DROP `reply_to`");
+
+	}
+
+}
--- themes/default/article.php
+++ themes/default/article.php
@@ -13,51 +13,79 @@
 			</section>
 		</section>
 
-		<?php if(comments_open()): ?>
-		<section class="comments">
-			<?php if(has_comments()): ?>
+<?php if(comments_open()): ?>
+	<section class="comments">
+		<?php if(has_comments()): ?>
 			<ul class="commentlist">
 				<?php $i = 0; while(comments()): $i++; ?>
-				<li class="comment" id="comment-<?php echo comment_id(); ?>">
-					<div class="wrap">
-						<h2><?php echo comment_name(); ?></h2>
-						<time><?php echo relative_time(comment_time()); ?></time>
+					<li class="comment" id="comment-<?php echo comment_id(); ?>">
+						<div class="wrap">
+							<h2><?php echo comment_name(); ?></h2>
+							<time><?php echo relative_time(comment_time()); ?></time>
 
-						<div class="content">
-							<?php echo comment_text(); ?>
+							<div class="content">
+								<?php echo comment_text(); ?>
+							</div>
+
+                        <span class="counter">
+                            <input class="btn" type="button" value="Reply" />
+	                        <?php echo $i; ?>
+                        </span>
 						</div>
+						<?php if(has_replies()) : ?>
+							<ul>
+								<?php $j = 0; while(replies()) : $j++; ?>
+									<li class="comment reply" id="comment-<?php echo reply_id(); ?>">
+										<div class="wrap">
+											<h2><?php echo reply_name(); ?></h2>
+											<time><?php echo relative_time(reply_time()); ?></time>
 
-						<span class="counter"><?php echo $i; ?></span>
-					</div>
-				</li>
+											<div class="content">
+												<?php echo reply_text(); ?>
+											</div>
+
+											<span class="counter"><?php echo "{$i}.{$j}"; ?></span>
+										</div>
+									</li>
+								<?php endwhile; ?>
+							</ul>
+						<?php endif; ?>
+					</li>
 				<?php endwhile; ?>
 			</ul>
-			<?php endif; ?>
-
-			<form id="comment" class="commentform wrap" method="post" action="<?php echo comment_form_url(); ?>#comment">
-				<?php echo comment_form_notifications(); ?>
-
-				<p class="name">
-					<label for="name">Your name:</label>
-					<?php echo comment_form_input_name('placeholder="Your name"'); ?>
-				</p>
-
-				<p class="email">
-					<label for="email">Your email address:</label>
-					<?php echo comment_form_input_email('placeholder="Your email (won’t be published)"'); ?>
-				</p>
-
-				<p class="textarea">
-					<label for="text">Your comment:</label>
-					<?php echo comment_form_input_text('placeholder="Your comment"'); ?>
-				</p>
-
-				<p class="submit">
-					<?php echo comment_form_button(); ?>
-				</p>
-			</form>
-
-		</section>
 		<?php endif; ?>
+
+		<form id="comment" class="commentform wrap" method="post" action="<?php echo comment_form_url(); ?>#comment">
+
+			<div id="comment-reply-notification">
+				<div>You are replying to @<span id="comment-reply-username">nobody</span>.</div>
+				<span id="cancel-reply" title="cancel">&times;</span>
+			</div>
+
+			<?php echo comment_form_notifications(); ?>
+
+			<p class="name">
+				<label for="name">Your name:</label>
+				<?php echo comment_form_input_name('placeholder="Your name"'); ?>
+			</p>
+
+			<p class="email">
+				<label for="email">Your email address:</label>
+				<?php echo comment_form_input_email('placeholder="Your email (won’t be published)"'); ?>
+			</p>
+
+			<p class="textarea">
+				<label for="text">Your comment:</label>
+				<?php echo comment_form_input_text('placeholder="Your comment"'); ?>
+			</p>
+
+			<p class="submit">
+				<?php echo comment_form_reply_to(); ?>
+				<?php echo comment_form_button(); ?>
+			</p>
+		</form>
+
+	</section>
+<?php endif; ?>
 
 <?php theme_include('footer'); ?>
