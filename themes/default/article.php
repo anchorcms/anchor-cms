794007374c67968b43008d0f01d129ed3519cae0
Devon Hazelett <xdev@devxdev.com>
2014-10-19 19:17:06 -0700

    Fix PHPStorm indetation clobbering...

--- themes/default/article.php
+++ themes/default/article.php
@@ -1,91 +1,91 @@
 <?php theme_include('header'); ?>
 
-		<section class="content wrap" id="article-<?php echo article_id(); ?>">
-			<h1><?php echo article_title(); ?></h1>
+        <section class="content wrap" id="article-<?php echo article_id(); ?>">
+            <h1><?php echo article_title(); ?></h1>
 
-			<article>
-				<?php echo article_markdown(); ?>
-			</article>
+            <article>
+                <?php echo article_markdown(); ?>
+            </article>
 
-			<section class="footnote">
-				<!-- Unfortunately, CSS means everything's got to be inline. -->
-				<p>This article is my <?php echo numeral(article_number(article_id()), true); ?> oldest. It is <?php echo count_words(article_markdown()); ?> words long<?php if(comments_open()): ?>, and it’s got <?php echo total_comments() . pluralise(total_comments(), ' comment'); ?> for now.<?php endif; ?> <?php echo article_custom_field('attribution'); ?></p>
-			</section>
-		</section>
+            <section class="footnote">
+                <!-- Unfortunately, CSS means everything's got to be inline. -->
+                <p>This article is my <?php echo numeral(article_number(article_id()), true); ?> oldest. It is <?php echo count_words(article_markdown()); ?> words long<?php if(comments_open()): ?>, and it’s got <?php echo total_comments() . pluralise(total_comments(), ' comment'); ?> for now.<?php endif; ?> <?php echo article_custom_field('attribution'); ?></p>
+            </section>
+        </section>
 
-<?php if(comments_open()): ?>
-	<section class="comments">
-		<?php if(has_comments()): ?>
-			<ul class="commentlist">
-				<?php $i = 0; while(comments()): $i++; ?>
-					<li class="comment" id="comment-<?php echo comment_id(); ?>">
-						<div class="wrap">
-							<h2><?php echo comment_name(); ?></h2>
-							<time><?php echo relative_time(comment_time()); ?></time>
+        <?php if(comments_open()): ?>
+        <section class="comments">
+            <?php if(has_comments()): ?>
+            <ul class="commentlist">
+                <?php $i = 0; while(comments()): $i++; ?>
+                <li class="comment" id="comment-<?php echo comment_id(); ?>">
+                    <div class="wrap">
+                        <h2><?php echo comment_name(); ?></h2>
+                        <time><?php echo relative_time(comment_time()); ?></time>
 
-							<div class="content">
-								<?php echo comment_text(); ?>
-							</div>
+                        <div class="content">
+                            <?php echo comment_text(); ?>
+                        </div>
 
                         <span class="counter">
                             <input class="btn" type="button" value="Reply" />
-	                        <?php echo $i; ?>
+                            <?php echo $i; ?>
                         </span>
-						</div>
-						<?php if(has_replies()) : ?>
-							<ul>
-								<?php $j = 0; while(replies()) : $j++; ?>
-									<li class="comment reply" id="comment-<?php echo reply_id(); ?>">
-										<div class="wrap">
-											<h2><?php echo reply_name(); ?></h2>
-											<time><?php echo relative_time(reply_time()); ?></time>
+                    </div>
+                    <?php if(has_replies()) : ?>
+                    <ul>
+                        <?php $j = 0; while(replies()) : $j++; ?>
+                        <li class="comment reply" id="comment-<?php echo reply_id(); ?>">
+                            <div class="wrap">
+                                <h2><?php echo reply_name(); ?></h2>
+                                <time><?php echo relative_time(reply_time()); ?></time>
 
-											<div class="content">
-												<?php echo reply_text(); ?>
-											</div>
+                                <div class="content">
+                                    <?php echo reply_text(); ?>
+                                </div>
 
-											<span class="counter"><?php echo "{$i}.{$j}"; ?></span>
-										</div>
-									</li>
-								<?php endwhile; ?>
-							</ul>
-						<?php endif; ?>
-					</li>
-				<?php endwhile; ?>
-			</ul>
-		<?php endif; ?>
+                                <span class="counter"><?php echo "{$i}.{$j}"; ?></span>
+                            </div>
+                        </li>
+                        <?php endwhile; ?>
+                    </ul>
+                    <?php endif; ?>
+                </li>
+                <?php endwhile; ?>
+            </ul>
+            <?php endif; ?>
 
-		<form id="comment" class="commentform wrap" method="post" action="<?php echo comment_form_url(); ?>#comment">
+            <form id="comment" class="commentform wrap" method="post" action="<?php echo comment_form_url(); ?>#comment">
 
-			<div id="comment-reply-notification">
-				<div>You are replying to @<span id="comment-reply-username">nobody</span>.</div>
-				<span id="cancel-reply" title="cancel">&times;</span>
-			</div>
+                <div id="comment-reply-notification">
+                    <div>You are replying to @<span id="comment-reply-username">nobody</span>.</div>
+                    <span id="cancel-reply" title="cancel">&times;</span>
+                </div>
 
-			<?php echo comment_form_notifications(); ?>
+                <?php echo comment_form_notifications(); ?>
 
-			<p class="name">
-				<label for="name">Your name:</label>
-				<?php echo comment_form_input_name('placeholder="Your name"'); ?>
-			</p>
+                <p class="name">
+                    <label for="name">Your name:</label>
+                    <?php echo comment_form_input_name('placeholder="Your name"'); ?>
+                </p>
 
-			<p class="email">
-				<label for="email">Your email address:</label>
-				<?php echo comment_form_input_email('placeholder="Your email (won’t be published)"'); ?>
-			</p>
+                <p class="email">
+                    <label for="email">Your email address:</label>
+                    <?php echo comment_form_input_email('placeholder="Your email (won’t be published)"'); ?>
+                </p>
 
-			<p class="textarea">
-				<label for="text">Your comment:</label>
-				<?php echo comment_form_input_text('placeholder="Your comment"'); ?>
-			</p>
+                <p class="textarea">
+                    <label for="text">Your comment:</label>
+                    <?php echo comment_form_input_text('placeholder="Your comment"'); ?>
+                </p>
 
-			<p class="submit">
-				<?php echo comment_form_reply_to(); ?>
-				<?php echo comment_form_button(); ?>
-			</p>
-		</form>
+                <p class="submit">
+                    <?php echo comment_form_reply_to(); ?>
+                    <?php echo comment_form_button(); ?>
+                </p>
+            </form>
 
-	</section>
-<?php endif; ?>
+        </section>
+        <?php endif; ?>
 
 <?php theme_include('footer'); ?>
--- themes/default/js/main.js
+++ themes/default/js/main.js
@@ -1,114 +1,114 @@
 var Anchor = {
-	init: function() {
-		Anchor.slidey = $('.slidey');
-		Anchor.keys = [];
+    init: function() {
+        Anchor.slidey = $('.slidey');
+        Anchor.keys = [];
 
-		//  Uh, bind to the resizing of the window?
-		$(window).resize(Anchor.bindResize).trigger('resize');
+        //  Uh, bind to the resizing of the window?
+        $(window).resize(Anchor.bindResize).trigger('resize');
 
-		// Re-/Set keys
-		$(window).on('keyup', Anchor.keyup);
-		$(window).on('keydown', Anchor.keydown);
+        // Re-/Set keys
+        $(window).on('keyup', Anchor.keyup);
+        $(window).on('keydown', Anchor.keydown);
 
-		//  Set up the toggle link
-		Anchor.linky = $('.linky').on('click', Anchor.toggleSlidey);
+        //  Set up the toggle link
+        Anchor.linky = $('.linky').on('click', Anchor.toggleSlidey);
 
-		//  Hide the thingymabob
-		setTimeout(function() {
-			//  Set up the slidey panel
-			Anchor.hideSlidey();
+        //  Hide the thingymabob
+        setTimeout(function() {
+            //  Set up the slidey panel
+            Anchor.hideSlidey();
 
-			$('body').addClass('js-enabled');
-		}, 10);
+            $('body').addClass('js-enabled');
+        }, 10);
 
-		//  Listen for search link
-		$('a[href="#search"]').click(function() {
-			if(!Anchor.linky.hasClass('active')) {
-				return Anchor.toggleSlidey.call(Anchor.linky);
-			}
-		});
+        //  Listen for search link
+        $('a[href="#search"]').click(function() {
+            if(!Anchor.linky.hasClass('active')) {
+                return Anchor.toggleSlidey.call(Anchor.linky);
+            }
+        });
 
-		$('.comment .btn').on('click', Anchor.reply);
-		$('#cancel-reply').on('click', Anchor.replyCancel);
+        $('.comment .btn').on('click', Anchor.reply);
+        $('#cancel-reply').on('click', Anchor.replyCancel);
 
-	},
+    },
 
-	keyup: function(event) {
-		Anchor.keys[event.keyCode] = false;
-	},
+    keyup: function(event) {
+        Anchor.keys[event.keyCode] = false;
+    },
 
-	keydown: function(event) {
-		Anchor.keys[event.keyCode] = true;
+    keydown: function(event) {
+        Anchor.keys[event.keyCode] = true;
 
-		// ctrl + shift + f => show Slidey and/or focus search bar
-		if(Anchor.keys[17] && Anchor.keys[16] && Anchor.keys[70]) {
-			event.preventDefault();
+        // ctrl + shift + f => show Slidey and/or focus search bar
+        if(Anchor.keys[17] && Anchor.keys[16] && Anchor.keys[70]) {
+            event.preventDefault();
 
-			Anchor.showSlidey.call(Anchor.linky);
-			$('input[type="search"]').focus();
-		}
+            Anchor.showSlidey.call(Anchor.linky);
+            $('input[type="search"]').focus();
+        }
 
-		// esc => hide Slidey
-		if(Anchor.keys[27]) {
-			event.preventDefault();
+        // esc => hide Slidey
+        if(Anchor.keys[27]) {
+            event.preventDefault();
 
-			Anchor.hideSlidey();
-			$('input[type="search"]').blur();
-		}
-	},
+            Anchor.hideSlidey();
+            $('input[type="search"]').blur();
+        }
+    },
 
-	hideSlidey: function() {
-		Anchor.slidey.css('margin-top', this._slideyHeight);
-		Anchor.linky && Anchor.linky.removeClass('active');
+    hideSlidey: function() {
+        Anchor.slidey.css('margin-top', this._slideyHeight);
+        Anchor.linky && Anchor.linky.removeClass('active');
 
-		return this;
-	},
+        return this;
+    },
 
-	showSlidey: function() {
-		Anchor.slidey.css('margin-top', 0);
-		Anchor.linky && Anchor.linky.addClass('active');
+    showSlidey: function() {
+        Anchor.slidey.css('margin-top', 0);
+        Anchor.linky && Anchor.linky.addClass('active');
 
-		return this;
-	},
+        return this;
+    },
 
-	toggleSlidey: function() {
-		var self = Anchor;
-		var me = $(this);
+    toggleSlidey: function() {
+        var self = Anchor;
+        var me = $(this);
 
-		me.toggleClass('active');
-		self.slidey.css('margin-top', me.hasClass('active') ? 0 : self._slideyHeight);
+        me.toggleClass('active');
+        self.slidey.css('margin-top', me.hasClass('active') ? 0 : self._slideyHeight);
 
-		return false;
-	},
+        return false;
+    },
 
-	bindResize: function() {
-		Anchor._slideyHeight = -(Anchor.slidey.height() + 1);
-		Anchor.hideSlidey();
-	},
+    bindResize: function() {
+        Anchor._slideyHeight = -(Anchor.slidey.height() + 1);
+        Anchor.hideSlidey();
+    },
 
-	reply: function() {
+    reply: function() {
 
-		var
-			that = $(this),
-			comment = that.parents('.comment')[0]
-			;
+        var
+            that = $(this),
+            comment = that.parents('.comment')[0]
+        ;
 
-		$('#reply_to').val(comment.id.replace(/comment\-/, ''));
-		$('#comment input').eq(0).focus();
+        $('#reply_to').val(comment.id.replace(/comment\-/, ''));
+        $('#comment input').eq(0).focus();
 
 
-		$('#comment-reply-notification').addClass('active');
-		$('#comment-reply-username').text($(comment).find('h2').text());
+        $('#comment-reply-notification').addClass('active');
+        $('#comment-reply-username').text($(comment).find('h2').text());
 
-	},
+    },
 
-	replyCancel: function() {
+    replyCancel: function() {
 
-		$('#reply_to').val(0);
-		$('#comment-reply-notification').removeClass('active');
-		$('#comment-reply-username').text('nobody');
+        $('#reply_to').val(0);
+        $('#comment-reply-notification').removeClass('active');
+        $('#comment-reply-username').text('nobody');
 
-	}
+    }
 };
 
 //  And bind loading
