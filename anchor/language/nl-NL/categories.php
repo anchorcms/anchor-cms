fbcdaacfc1987cf0f2fc2afcad17811264e2d62b
Sten Van Houwelingen <jahetzalwell@gmail.com>
2014-11-29 12:28:11 +0100

    Begun to add Dutch language

---
+++ anchor/language/nl-NL/categories.php
@@ -0,0 +1,20 @@
+<?php
+return array(
+	'category' => 'Categorie',
+	'categories' => 'Categorieën',
+	'create_category' => 'Creëer een nieuwe categorie',
+	'edit_category' => 'Bewerk &ldquo;%s&rdquo;',
+	// form fields
+	'title' => 'Titel',
+	'title_explain' => 'Uw categorie titel.',
+	'title_missing' => 'Voer een titel in, het moet minimaal 3 tekens lang zijn.',
+	'slug' => 'Slug',
+	'slug_explain' => 'De slug voor uw categorie.',
+	'description' => 'Beschrijving',
+	'description_explain' => 'Waar gaat uw categorie over.',
+	// messages
+	'created' => 'Uw nieuwe categorie is toegevoegd.',
+	'updated' => 'Uw categorie is bijgewerkt.',
+	'deleted' => 'Uw categorie is verwijderd.',
+	'delete_error' => 'U moet minstens één categorie hebben.',
+);
