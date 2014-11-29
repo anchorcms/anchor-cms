2018f8c5b4b3b221e37a8ac8d2f2ea8a10611777
Sten Van Houwelingen <jahetzalwell@gmail.com>
2014-11-29 13:25:09 +0100

    Create global.php

---
+++ anchor/language/nl-NL/global.php
@@ -0,0 +1,64 @@
+<?php
+/*
+ * Global is to replace common.php
+ *
+ * This global file will provide the majority of the translations in anchor
+ * to remove duplicate translations in section specific language files.
+ */
+return array(
+	// words
+	'save' => 'Opslaan',
+	'delete' => 'Verwijderen',
+	'update' => 'Bijwerken',
+	'edit' => 'Bewerken',
+	'editing' => 'Berken',
+	'create' => 'Maak',
+	'created' => 'Gemaakt',
+	'submit' => 'Indienen',
+	'close' => 'Sluiten',
+	'status' => 'Status',
+	'manage' => 'Beheer',
+	'reset' => 'Reset',
+	'all' => 'Alles',
+	// pagination
+	'next' => 'Volgende',
+	'previous' => 'Vorige',
+	'first' => 'Eerste',
+	'last' => 'Laaste',
+	// statuses
+	'draft' => 'Concept',
+	'archived' => 'Gearchiveerd',
+	'published' => 'Gepubliceerd',
+	'pending' => 'In afwachting',
+	'approved' => 'Goedgekeurd',
+	'spam' => 'Spam',
+	'inactive' => 'Niet actief',
+	'active' => 'Actief',
+	// roles
+	'administrator' => 'Admin',
+	'editor' => 'Editor',
+	'user' => 'Gebruiker',
+	'log_in' => 'Log in',
+	'login' => 'Login',
+	'log_out' => 'Log uit',
+	'logout' => 'Loguit',
+	// pharses
+	'visit_your_site' => 'Bezoek uw site',
+	'powered_by_anchor' => 'Powered by Anchor, Versie %s',
+	'make_blogging_beautiful' => 'Maak bloggen prachtig.',
+	// intro
+	'welcome_to_anchor' => 'Welkom bij Anchor',
+	'welcome_to_anchor_lets_go' => 'Welkom bij Anchor. Laten we beginnen.',
+	'run_the_installer' => 'Start het installatieprogramma',
+	// upgrade
+	'upgrade' => 'Upgrade',
+	'good_news' => 'Goed Nieuws!',
+	'new_version_available' => 'Er is een nieuwe versie van Anchor beschikbaar.',
+	'download_now' => 'Download het nu',
+	'upgrade_later' => 'Upgrade later',
+	// debug profiler
+	'profile' => 'Profiel',
+	'profile_memory_usage' => 'Mijn totale geheugengebruik',
+	// messages
+	'confirm_delete' => 'Weet u zeker dat u dat wilt verwijderen? Dit kan niet ongedaan worden gemaakt!'
+);
