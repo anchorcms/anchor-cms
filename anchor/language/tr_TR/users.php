b223defddaeffbaea37c3d32a65b3c2d9075b200
Yuksel <yuks86@yahoo.com>
2014-12-13 12:05:40 +0100

    Added turkish language

---
+++ anchor/language/tr_TR/users.php
@@ -0,0 +1,67 @@
+<?php
+
+return array(
+
+	'users' => 'Kullanıcılar',
+
+	'create_user' => 'Yeni kullanıcı oluştur',
+	'add_user' => 'Yeni Kullanıcı Ekle',
+	'editing_user' => '&ldquo;%s&rdquo; Profil Düzenle',
+	'remembered' => 'Şifremi biliyorum',
+	'forgotten_password' => 'Şifreni mi unuttun?',
+
+	// roles
+	'administrator' => 'Admin',
+	'administrator_explain' => '',
+
+	'editor' => 'Editör',
+	'editor_explain' => '',
+
+	'user' => 'Kullanıcı',
+	'user_explain' => '',
+
+	// form fields
+	'real_name' => 'Gerçek İsim',
+	'real_name_explain' => '',
+
+	'bio' => 'Biyografi',
+	'bio_explain' => '',
+
+	'status' => 'Durum',
+	'status_explain' => '',
+
+	'role' => 'Rolü',
+	'role_explain' => '',
+
+	'username' => 'Kullanıcı Adı',
+	'username_explain' => '',
+	'username_missing' => 'Lütfen bir kullanıcı adı yaz. En az %s karakter uzunluğunda olmalı.',
+
+	'password' => 'Şifre',
+	'password_explain' => '',
+	'password_too_short' => 'Şifre en az %s karakter uzunluğunda olmalı',
+
+	'new_password' => 'Yeni Şifre',
+
+	'email' => 'E-Posta',
+	'email_explain' => '',
+	'email_missing' => 'Lütfen geçerli bir e-posta adresi yaz',
+	'email_not_found' => 'Profil bulunamadı',
+
+	// messages
+	'updated' => 'Kullanıcı profili düzenlendi.',
+	'created' => 'Kullanıcı profili oluşturuldu.',
+	'deleted' => 'Kullanıcı profili silindi.',
+	'delete_error' => 'Kendi profilini silemezsin',
+	'login_error' => 'Kullanıcı adı veya şifre yanlış.',
+	'logout_notice' => 'Çıkış yaptın.',
+	'recovery_sent' => 'Şifre değişimini onaylamak için bir e-posta yolladık.',
+	'recovery_expired' => 'Şifre kurtarma zaman aşımına uğradı, lütfen tekrar deneyin.',
+	'password_reset' => 'Yeni şifre kaydedildi. Hemen giriş yap!',
+
+	// password recovery email
+	'recovery_subject' => 'Şifre Sıfırla',
+	'recovery_message' => 'Şifrenin sıfırlanmasını istedin.' .
+		'Devam etmek için alttaki linki takip et.' . PHP_EOL . '%s',
+
+);
