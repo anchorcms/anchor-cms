92a56d27441f64f37c737d91c93fc3807b4e919e
Yuksel <yuks86@yahoo.com>
2014-12-13 12:03:28 +0100

    Added turkish language

---
+++ anchor/language/tr_TR/comments.php
@@ -0,0 +1,35 @@
+<?php
+
+return array(
+
+	'comments' => 'Yorumlar',
+	'nocomments_desc' => 'Henüz yorum yok.',
+	'editing_comment' => 'Yorum Düzenle',
+	'view_comment' => 'View comment',
+
+	// form fields
+	'name' => 'İsim',
+	'name_explain' => 'Yazar ismi',
+	'name_missing' => 'Lütfen bir isim yaz',
+
+	'email' => 'Email Adresi',
+	'email_explain' => 'Yazar email adresi',
+	'email_missing' => 'Lütfen geçerli bir e-mail adresi yazınız', // frontend message (appears on your site!)
+
+	'text' => 'Yorum',
+	'text_explain' => '',
+	'text_missing' => 'Lütfen yorumunuzu yazınız', // frontend message (appears on your site!)
+
+	'status' => 'Durum',
+	'status_explain' => '',
+
+	// messages
+	'created' => 'Yorumunuz eklendi', // frontend message (appears on your site!)
+	'updated' => 'Yorum düzenlendi',
+	'deleted' => 'Yorum silindi',
+
+	// email notification
+	'notify_subject' => 'Yeni yorum eklendi',
+	'nofity_heading' => 'Sitene yeni bir yorum gönderildi.'
+
+);
