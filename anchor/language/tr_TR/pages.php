8cdf498375f38e201be103bf545eb0deecbe1e8e
Yuksel <yuks86@yahoo.com>
2014-12-13 12:05:10 +0100

    Added turkish language

---
+++ anchor/language/tr_TR/pages.php
@@ -0,0 +1,45 @@
+<?php
+
+return array(
+
+	'pages' => 'Sayfalar',
+
+	'create_page' => 'Yeni sayfa oluştur',
+	'nopages_desc' => 'Hiç sayfa yok.',
+	'redirect' => 'Yönlendirme',
+
+	// form fields
+	'redirect_url' => 'Yönlendirme URL Adresi',
+	'redirect_missing' => 'Lütfen geçerli bir URL yaz',
+
+	'title' => 'Sayfa Başlığı',
+	'title_explain' => '',
+	'title_missing' => 'Lütfen bir sayfa başlığı yaz',
+
+	'content' => 'İçerik',
+	'content_explain' => 'Sayfanın içeriği. Markdown kullanılabilir.',
+
+	'show_in_menu' => 'Menüde Göster',
+	'show_in_menu_explain' => '',
+
+	'name' => 'İsim',
+	'name_explain' => '',
+
+	'slug' => 'Kısa Başlık',
+	'slug_explain' => '“slug” yazı isminin URL versiyonudur. Sadece ascii karakterler kullanılabilir.',
+	'slug_missing' => 'Lütfen bir kısa başlık yaz, sadece ascii karakterler kullanılabilir',
+	'slug_duplicate' => 'Kısa başlık zaten kullanımda',
+	'slug_invalid' => 'Kısa başlık harf içermeli',
+
+	'status' => 'Durum',
+	'status_explain' => '',
+
+	'parent' => 'Ebeveyn',
+	'parent_explain' => '',
+
+	// messages
+	'updated' => 'Sayfa düzenlendi.',
+	'created' => 'Sayfa oluşturuldu.',
+	'deleted' => 'Sayfa silindi.'
+
+);
