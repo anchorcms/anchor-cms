6196035b86e77b09d0ac9efb3f91d8d1acfee564
Yuksel <yuks86@yahoo.com>
2014-12-13 12:03:01 +0100

    Added turkish language

---
+++ anchor/language/tr_TR/categories.php
@@ -0,0 +1,28 @@
+<?php
+
+return array(
+
+	'category' => 'Kategori',
+	'categories' => 'Kategoriler',
+
+	'create_category' => 'Yeni kategori oluştur',
+	'edit_category' => '&ldquo;%s&rdquo; Düzenle',
+
+	// form fields
+	'title' => 'Başlık',
+	'title_explain' => 'Kategorinin ismi.',
+	'title_missing' => 'Lütfen bir başlık yaz',
+
+	'slug' => 'Kısa Başlık',
+	'slug_explain' => '“slug” yazı isminin URL versiyonudur.',
+
+	'description' => 'Açıklama',
+	'description_explain' => 'Kategorinin açıklaması, ne hakkında olduğu.',
+
+	// messages
+	'created' => 'Yeni kategorin eklendi.',
+	'updated' => 'Kategorin düzenlendi.',
+	'deleted' => 'Kategorin silindi.',
+	'delete_error' => 'En az bir kategorinin olması gerekiyor.',
+
+);
