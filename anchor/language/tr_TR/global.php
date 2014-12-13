9ca8e45c265c0312d8f8b5fc2c95e5843ac5d0e7
Yuksel <yuks86@yahoo.com>
2014-12-13 12:04:23 +0100

    Added turkish language

---
+++ anchor/language/tr_TR/global.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * Global is to replace common.php
+ *
+ * This global file will provide the majority of the translations in anchor
+ * to remove duplicate translations in section specific language files.
+ */
+return array(
+
+	// words
+	'save' => 'Kaydet',
+	'delete' => 'Sil',
+	'update' => 'Güncelle',
+	'edit' => 'Değiştir',
+	'editing' => 'Değiştir',
+	'create' => 'Oluştur',
+	'created' => 'Oluşturuldu',
+	'submit' => 'Gönder',
+	'close' => 'Kapat',
+	'status' => 'Durum',
+	'manage' => 'Yönet',
+	'reset' => 'Sıfırla',
+	'all' => 'Hepsi',
+
+	// pagination
+	'next' => 'Sonraki',
+	'previous' => 'Önceki',
+	'first' => 'İlk',
+	'last' => 'Son',
+
+	// statuses
+	'draft' => 'Taslak',
+	'archived' => 'Arşivlendi',
+	'published' => 'Yayınlandı',
+	'pending' => 'Beklemede',
+	'approved' => 'Onaylandı',
+	'spam' => 'İstenmeyen',
+
+	'inactive' => 'Pasif',
+	'active' => 'Aktif',
+
+	// roles
+	'administrator' => 'Admin',
+	'editor' => 'Editör',
+	'user' => 'Kullanıcı',
+
+	'log_in' => 'Giriş',
+	'login' => 'Giriş',
+	'log_out' => 'Çıkış',
+	'logout' => 'Çıkış',
+
+	// pharses
+	'visit_your_site' => 'Siteni ziyaret et',
+	'powered_by_anchor' => 'Powered by Anchor, version %s',
+	'make_blogging_beautiful' => 'Make blogging beautiful.',
+
+	// intro
+	'welcome_to_anchor' => 'Anchor\'a Hoşgeldin',
+	'welcome_to_anchor_lets_go' => 'Anchor\'a hoşgeldin. Hadi başlayalım.',
+	'run_the_installer' => 'Run the installer',
+
+	// upgrade
+	'upgrade' => 'Yükselt',
+	'good_news' => 'İyi Haberler Var!',
+	'new_version_available' => 'Yeni bir Anchor sürümü var.',
+	'download_now' => 'Şimdi İndir',
+	'upgrade_later' => 'Sonra Yükselt',
+
+	// debug profiler
+	'profile' => 'Profil',
+	'profile_memory_usage' => 'Toplam bellek kullanımı',
+
+	// messages
+	'confirm_delete' => 'Bunu silmek istediğine emin misin? Geri dönüşü yok!'
+
+);
