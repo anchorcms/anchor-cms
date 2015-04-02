4ccbca5e7b1c308ad889a021bdd1ee6751ecdab4
CraigChilds94 <childscraig17@gmail.com>
2015-04-02 11:42:34 +0100

    :memo: Bump V num in install globals

--- install/index.php
+++ install/index.php
@@ -23,7 +23,7 @@
 
 define('DS', DIRECTORY_SEPARATOR);
 define('ENV', getenv('APP_ENV'));
-define('VERSION', '0.9');
+define('VERSION', '0.10');
 
 define('PATH', dirname(dirname(__FILE__)) . DS);
 define('APP', PATH . 'install' . DS);
