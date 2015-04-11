fdfd5697ad73417cc107c343eab019c6771c95b2
corpsee <poisoncorpsee@gmail.com>
2015-04-11 08:27:21 +0600

    Added ru_RU translation

---
+++ anchor/language/ru_RU/global.php
@@ -0,0 +1,80 @@
+<?php
+
+/*
+ * Global is to replace common.php
+ *
+ * This global file will provide the majority of the translations in anchor
+ * to remove duplicate translations in section specific language files.
+ */
+return array(
+
+	// words
+	'save' => 'Сохранить',
+	'saving' => 'Сохранение...',
+	'delete' => 'Удалить',
+	'deleting' => 'Удаление...',
+	'update' => 'Обновить',
+	'updating' => 'Обновление...',
+	'edit' => 'Редактировать',
+	'editing' => 'Редактирование',
+	'create' => 'Создать',
+	'created' => 'Создан',
+	'submit' => 'Отправить',
+	'close' => 'Закрыть',
+	'status' => 'Статус',
+	'manage' => 'Управление',
+	'reset' => 'Сбросить',
+	'all' => 'Все',
+
+	// pagination
+	'next' => 'Следующая',
+	'previous' => 'Предыдущая',
+	'first' => 'Первая',
+	'last' => 'Посвледняя',
+
+	// statuses
+	'draft' => 'Черновик',
+	'archived' => 'Архивная',
+	'published' => 'Опубликованная',
+	'pending' => 'Ожидающая',
+	'approved' => 'Проверенная',
+	'spam' => 'Спам',
+
+	'inactive' => 'Неактивная',
+	'active' => 'Активная',
+
+	// roles
+	'administrator' => 'Админ',
+	'editor' => 'Редактор',
+	'user' => 'Пользователь',
+
+	'log_in' => 'Войти',
+	'login' => 'Логин',
+	'log_out' => 'Выйти',
+	'logout' => 'Выйти',
+
+	// phrases
+	'visit_your_site' => 'Посетить ваш сайт',
+	'powered_by_anchor' => 'Работает на Anchor, версия %s',
+	'make_blogging_beautiful' => 'Make blogging beautiful',
+
+	// intro
+	'welcome_to_anchor' => 'Добро пожаловать в Anchor',
+	'welcome_to_anchor_lets_go' => 'Добро пожаловать в Anchor. Поехали.',
+	'run_the_installer' => 'Run the installer',
+
+	// upgrade
+	'upgrade' => 'Обновить',
+	'good_news' => 'Отличные новости!',
+	'new_version_available' => 'Новая версия Anchor доступна.',
+	'download_now' => 'Скачать сейчас',
+	'upgrade_later' => 'Обновить позже',
+
+	// debug profiler
+	'profile' => 'Профайл',
+	'profile_memory_usage' => 'Общее использование памяти',
+
+	// messages
+	'confirm_delete' => 'Вы уверены что хотите удалить? Это дейстие нельзя будет отменить!'
+
+);
---
+++ anchor/language/ru_RU/metadata.php
@@ -0,0 +1,48 @@
+<?php
+
+return array(
+
+	'metadata' => 'Свойства сайта',
+	'metadata_desc' => 'Управление свойствами сайта',
+
+	'comment_settings' => 'Комментарии',
+	'theme_settings' => 'Внешний вид',
+
+	// form fields
+	'sitename' => 'Название сайта',
+	'sitename_explain' => '',
+	'sitename_missing' => 'Вашему сайту нужно название!',
+
+	'sitedescription' => 'Описание сайта',
+	'sitedescription_explain' => '',
+	'sitedescription_missing' => 'Вашему сайту нужно описание!',
+
+	'homepage' => 'Домашняя страница',
+	'homepage_explain' => '',
+
+	'postspage' => 'Страница записей',
+	'postspage_explain' => '',
+
+	'posts_per_page' => 'Записей на страницу',
+	'posts_per_page_explain' => '',
+
+	'show_all_posts' => 'Показывать все записи',
+	'show_all_posts_explain' => 'Показывать все записи без пагинатора (это может быть медленным)',
+
+	'auto_publish_comments' => 'Авторазрешение комментариев',
+	'auto_publish_comments_explain' => '',
+
+	'comment_notifications' => 'Оповещение по email о новых комментариях',
+	'comment_notifications_explain' => '',
+
+	'comment_moderation_keys' => 'Ключевые слова для спама',
+	'comment_moderation_keys_explain' => 'Список ключевых слов перечисленных серез запятую.
+		Комментарий со словами из этого списка будет автоматически признан спамом.',
+
+	'current_theme' => 'Текущая тема',
+	'current_theme_explain' => '',
+
+	// messages
+	'updated' => 'Свойства были обновлены',
+
+);
