7df51ce2bd1ef0fafb1b6bff395c7744658d4a9a
Giacomo Cerquone <cerquone@gmail.com>
2015-07-14 23:35:51 +0200

    Fixed Timezone during installation | Issue #893
    Check if it's summer time. If so it subtracts 1.

--- install/run.php
+++ install/run.php
@@ -38,25 +38,24 @@ if($route = Arr::get($_GET, 'route', '/')) {
 	Helper functions
 */
 function timezones() {
+
 	$list = DateTimeZone::listIdentifiers();
-
 	$data = array();
-    foreach($list as $id => $zone){
-        $now = new DateTime(null, new DateTimeZone($zone));
-        $offset = $now->getOffset();
-        $offset_round = round(abs($offset / 3600), 2);
-        $minutes = fmod($offset_round, 1);
-
+    foreach($list as $id => $zone) {
+        $date= new DateTime(null, new DateTimeZone($zone));
+        $ds = $date->format('I');
+        $offset = $date->getOffset();
+        $gmt= round(abs($offset / 3600), 2);
+        $gmt = ($ds == 1 ? $gmt = $gmt-1 );
+        $minutes = fmod($gmt, 1);
         if($minutes == 0) {
-            $offset_label = $offset_round.'&nbsp;&nbsp;';
+            $offset_label = $gmt.'&nbsp;&nbsp;';
         } elseif ($minutes == 0.5) {
-            $offset_label = (int)$offset_round.'.30';
+            $offset_label = (int)$gmt.'.30';
         } elseif ($minutes == 0.75) {
-            $offset_label = (int)$offset_round.'.45';
+            $offset_label = (int)$gmt.'.45';
         }
-
         $sign = $offset > 0 ? '+' : '-';
-
         if($offset == 0) {
             $sign = ' ';
             $offset = '';
@@ -65,18 +64,13 @@ function timezones() {
         $data[$offset][$zone] = array('offset'=> $offset, 'label' => $label, 'timezone_id' => $zone);
     }
     ksort($data);
-
 	$timezones = array();
-
 	foreach($data as $offsets) {
-
 		ksort($offsets);
-
 		foreach($offsets as $zone) {
 			$timezones[] = $zone;
 		}
 	}
-
 	return $timezones;
 
 }
