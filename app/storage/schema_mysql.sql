65c192c81a70be86994d2b9e5b05baa17cbd6019
rwarasaurus <info@kieronwilson.co.uk>
2015-08-27 18:00:13 +0100

    clean up sql file

--- app/storage/schema_mysql.sql
+++ app/storage/schema_mysql.sql
@@ -1,110 +1,88 @@
-CREATE TABLE `anchor_categories` (
-  `id` int(6) NOT NULL AUTO_INCREMENT,
-  `title` varchar(150) NOT NULL,
-  `slug` varchar(40) NOT NULL,
-  `description` text NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE `{prefix}categories` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`title` VARCHAR NOT NULL,
+	`slug` VARCHAR NOT NULL,
+	`description` TEXT NOT NULL,
+	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-CREATE TABLE `anchor_comments` (
-  `id` int(6) NOT NULL AUTO_INCREMENT,
-  `post` int(6) NOT NULL,
-  `status` text NOT NULL,
-  `date` datetime NOT NULL,
-  `name` varchar(140) NOT NULL,
-  `email` varchar(140) NOT NULL,
-  `text` text NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `anchor_comments_status` (`status`(255)),
-  KEY `anchor_comments_post` (`post`)
+
+CREATE TABLE `{prefix}extend` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`type` TEXT NOT NULL,
+	`field` TEXT NOT NULL,
+	`key` VARCHAR NOT NULL,
+	`label` VARCHAR NOT NULL,
+	`attributes` TEXT NOT NULL,
+	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-CREATE TABLE `anchor_extend` (
-  `id` int(6) NOT NULL AUTO_INCREMENT,
-  `type` text NOT NULL,
-  `field` text NOT NULL,
-  `key` varchar(160) NOT NULL,
-  `label` varchar(160) NOT NULL,
-  `attributes` text NOT NULL,
-  PRIMARY KEY (`id`)
+
+CREATE TABLE `{prefix}meta` (
+	`key` VARCHAR NOT NULL,
+	`value` TEXT NOT NULL,
+	PRIMARY KEY (`key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-CREATE TABLE `anchor_meta` (
-  `key` varchar(140) NOT NULL,
-  `value` text NOT NULL,
-  PRIMARY KEY (`key`)
+
+CREATE TABLE `{prefix}page_meta` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`page` INT NOT NULL,
+	`extend` INT NOT NULL,
+	`data` TEXT NOT NULL,
+	PRIMARY KEY (`id`),
+	KEY `{prefix}page_meta_extend` (`extend`),
+	KEY `{prefix}page_meta_page` (`page`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-CREATE TABLE `anchor_page_meta` (
-  `id` int(6) NOT NULL AUTO_INCREMENT,
-  `page` int(6) NOT NULL,
-  `extend` int(6) NOT NULL,
-  `data` text NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `anchor_page_meta_extend` (`extend`),
-  KEY `anchor_page_meta_page` (`page`)
+
+CREATE TABLE `{prefix}pages` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`parent` INT NOT NULL,
+	`slug` VARCHAR NOT NULL,
+	`name` VARCHAR NOT NULL,
+	`title` VARCHAR NOT NULL,
+	`content` TEXT NOT NULL,
+	`html` TEXT NOT NULL,
+	`status` TEXT NOT NULL,
+	`redirect` TEXT NOT NULL,
+	`show_in_menu` INT NOT NULL,
+	`menu_order` INT NOT NULL,
+	PRIMARY KEY (`id`),
+	KEY `{prefix}pages_slug` (`slug`),
+	KEY `{prefix}pages_status` (`status`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-CREATE TABLE `anchor_pages` (
-  `id` int(6) NOT NULL AUTO_INCREMENT,
-  `parent` int(6) NOT NULL,
-  `slug` varchar(150) NOT NULL,
-  `name` varchar(64) NOT NULL,
-  `title` varchar(150) NOT NULL,
-  `content` text NOT NULL,
-  `status` text NOT NULL,
-  `redirect` text NOT NULL,
-  `show_in_menu` tinyint(1) NOT NULL,
-  `menu_order` int(4) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `anchor_pages_slug` (`slug`),
-  KEY `anchor_pages_status` (`status`(255))
+
+CREATE TABLE `{prefix}posts` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`title` VARCHAR NOT NULL,
+	`slug` VARCHAR NOT NULL,
+	`content` TEXT NOT NULL,
+	`html` TEXT NOT NULL,
+	`created` DATETIME NOT NULL,
+	`modified` DATETIME NOT NULL,
+	`author` INT NOT NULL,
+	`category` INT NOT NULL,
+	`status` TEXT NOT NULL,
+	PRIMARY KEY (`id`),
+	KEY `{prefix}posts_slug` (`slug`),
+	KEY `{prefix}posts_status` (`status`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-CREATE TABLE `anchor_plugins` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `path` text NOT NULL,
-  `name` text NOT NULL,
-  `description` text NOT NULL,
-  `version` text NOT NULL,
-  PRIMARY KEY (`id`)
+
+CREATE TABLE `{prefix}post_meta` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`post` INT NOT NULL,
+	`extend` INT NOT NULL,
+	`data` TEXT NOT NULL,
+	PRIMARY KEY (`id`),
+	KEY `{prefix}post_meta_extend` (`extend`),
+	KEY `{prefix}post_meta_post` (`post`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-CREATE TABLE `anchor_post_meta` (
-  `id` int(6) NOT NULL AUTO_INCREMENT,
-  `post` int(6) NOT NULL,
-  `extend` int(6) NOT NULL,
-  `data` text NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `post` (`post`),
-  KEY `extend` (`extend`),
-  KEY `anchor_post_meta_extend` (`extend`),
-  KEY `anchor_post_meta_post` (`post`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-CREATE TABLE `anchor_posts` (
-  `id` int(6) NOT NULL AUTO_INCREMENT,
-  `title` varchar(150) NOT NULL,
-  `slug` varchar(150) NOT NULL,
-  `description` text NOT NULL,
-  `html` mediumtext NOT NULL,
-  `css` text NOT NULL,
-  `js` text NOT NULL,
-  `created` datetime NOT NULL,
-  `author` int(6) NOT NULL,
-  `category` int(6) NOT NULL,
-  `status` text NOT NULL,
-  `comments` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `anchor_posts_slug` (`slug`),
-  KEY `anchor_posts_status` (`status`(255))
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-CREATE TABLE `anchor_sessions` (
-  `id` char(32) NOT NULL,
-  `expire` int(10) NOT NULL,
-  `data` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-CREATE TABLE `anchor_users` (
-  `id` int(6) NOT NULL AUTO_INCREMENT,
-  `username` varchar(100) NOT NULL,
-  `password` text NOT NULL,
-  `email` varchar(140) NOT NULL,
-  `real_name` varchar(140) NOT NULL,
-  `bio` text NOT NULL,
-  `status` text NOT NULL,
-  `role` text NOT NULL,
-  PRIMARY KEY (`id`)
+
+CREATE TABLE `{prefix}users` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`username` VARCHAR NOT NULL,
+	`password` TEXT NOT NULL,
+	`email` VARCHAR NOT NULL,
+	`real_name` VARCHAR NOT NULL,
+	`bio` TEXT NOT NULL,
+	`status` TEXT NOT NULL,
+	`role` TEXT NOT NULL,
+	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
