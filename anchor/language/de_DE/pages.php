f2cc7009ce565172b580b27deed1656dc024c393
Mo <Radiergummi@users.noreply.github.com>
2015-09-05 16:01:12 +0200

    Improved translation, added missing entries

--- anchor/language/de_DE/pages.php
+++ anchor/language/de_DE/pages.php
@@ -5,19 +5,19 @@ return array(
 	'pages' => 'Seiten',
 
 	'create_page' => 'Erstelle eine neue Seite',
-	'nopages_desc' => 'Du hast noch keine Seiten.',
+	'nopages_desc' => 'Du hast noch keine Seiten angelegt.',
 	'redirect' => 'Weiterleitung',
 
 	// form fields
-	'redirect_url' => 'Weiterleitungs Url',
-	'redirect_missing' => 'Bitte gib eine valide Url ein',
+	'redirect_url' => 'Weiterleitungs-URL',
+	'redirect_missing' => 'Bitte gib eine gültige URL ein',
 
 	'title' => 'Seitentitel',
 	'title_explain' => '',
 	'title_missing' => 'Bitte einen Titel für die Seite angeben',
 
 	'content' => 'Inhalt',
-	'content_explain' => 'Der Inhalt deiner Seite. Verwendet Markdown.',
+	'content_explain' => 'Der Inhalt deiner Seite (Verwendet Markdown).',
 
 	'show_in_menu' => 'Im Menü anzeigen',
 	'show_in_menu_explain' => '',
@@ -26,16 +26,19 @@ return array(
 	'name_explain' => '',
 
 	'slug' => 'Slug',
-	'slug_explain' => 'Slug uri um deine Seite anzuzeigen; sollte nur ascii Zeichen enthalten',
-	'slug_missing' => 'Bitte gib eine slug uri an: Slugs dürfen nur ascii Zeichen enthalten',
-	'slug_duplicate' => 'Slug existiert schon',
+	'slug_explain' => 'URL zu dieser Seite; sollte nur ASCII-Zeichen enthalten',
+	'slug_missing' => 'Bitte gib eine Slug-URL an: Slugs dürfen nur ASCII-Zeichen enthalten',
+	'slug_duplicate' => 'Slug existiert bereits',
 	'slug_invalid' => 'Slug muss Buchstaben enthalten',
 
 	'status' => 'Status',
 	'status_explain' => '',
 
-	'parent' => 'Elternelement',
+	'parent' => 'Übergeordnete Seite',
 	'parent_explain' => '',
+
+	'pagetype' => 'Seitentyp',
+	'pagetype_explain' => 'Gebe einen Typ für diese Seite an.',
 
 	// messages
 	'updated' => 'Deine Seite wurde aktualisiert.',
