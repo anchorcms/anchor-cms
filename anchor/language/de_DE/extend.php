824cf2df1251c0e1206bb2afb12057e4f30e715a
Mo <Radiergummi@users.noreply.github.com>
2015-09-05 15:59:36 +0200

    Improved translation, added missing entries

--- anchor/language/de_DE/extend.php
+++ anchor/language/de_DE/extend.php
@@ -5,60 +5,74 @@ return array(
 	'extend' => 'Erweiterungen',
 
 	'fields' => 'Benutzerdefinierte Felder',
-	'fields_desc' => 'Erstelle zusätzliche Felder',
+	'fields_desc' => 'Erstelle zusätzliche Eingabefelder',
 
-	'variables' => 'Seiten Variablen',
+	'pagetypes' => 'Benutzerdefinierte Seitentypen',
+	'pagetypes_desc' => 'Erstelle zusätzliche Seitentypen',
+
+	'variables' => 'Benutzerdefinierte Variablen',
 	'variables_desc' => 'Erstelle zusätzliche Metadaten',
 
 	'create_field' => 'Erstelle ein neues Feld',
-	'editing_custom_field' => 'Bearbeite das &ldquo;%s&rdquo; Feld',
-	'nofields_desc' => 'Noch keine Felder',
+	'editing_custom_field' => 'Bearbeite das Feld &ldquo;%s&rdquo;',
+	'nofields_desc' => 'Noch keine Felder angelegt',
 
 	'create_variable' => 'Erstelle eine neue Variable',
-	'editing_variable' => 'Bearbeite die &ldquo;%s&rdquo; Variable',
-	'novars_desc' => 'Noch keine Variablen',
+	'editing_variable' => 'Bearbeite die Variable &ldquo;%s&rdquo;',
+	'novars_desc' => 'Noch keine Variablen angelegt',
+
+	'create_pagetype' => 'Erstelle einen neuen Seitentyp',
+	'editing_pagetype' => 'Bearbeite den Seitentyp &ldquo;%s&rdquo;',
 
 	// form fields
 	'type' => 'Typ',
-	'type_explain' => 'Der Inhaltstyp zu dem du das Feld hinzufügen möchtest.',
+	'type_explain' => 'Die Art der Seite, für die dieses Feld verfügbar sein soll.',
+	'notypes_desc' => 'Es gibt noch keine Seitentypen',
+
+	'pagetype' => 'Seitentyp',
+	'pagetype_explain' => 'Der benutzerdefinierte Seitentyp, für den dieses Feld verfügbar sein soll.',
 
 	'field' => 'Feld',
-	'field_explain' => 'Html Input-Feld Typ',
+	'field_explain' => 'Art des Inhalts',
 
-	'key' => 'Unique Key',
-	'key_explain' => 'Der unique key für dieses Feld',
-	'key_missing' => 'Bitte gib einen unique key kein',
-	'key_exists' => 'Der Key ist schon in Benutzung',
+	'key' => 'Eindeutiger Name',
+	'key_explain' => 'Der eindeutige Name dieses Feldes',
+	'key_missing' => 'Bitte gib einen eindeutigen Namen ein',
+	'key_exists' => 'Dieses Feld existiert bereits',
 
 	'label' => 'Feldname',
 	'label_explain' => 'Angezeigter Feldname',
-	'label_missing' => 'Bitte einen Feldnamen angeben',
+	'label_missing' => 'Bitte einen Feldnamen eingeben',
 
 	'attribute_type' => 'Dateitypen',
-	'attribute_type_explain' => 'Komma-separierte Liste aller erlaubter Dateitypen: Feld leer lassen um alle zu erlauben.',
+	'attribute_type_explain' => 'Kommagetrennte Liste aller erlaubter Dateitypen (Feld leer lassen, um alle zu erlauben).',
 
 	// images
 	'attributes_size_width' => 'Maximale Bildbreite',
-	'attributes_size_width_explain' => 'Bilder werden angepasst wenn sie die maximale Bildbreite überschreiten',
+	'attributes_size_width_explain' => 'Bilder werden angepasst, wenn sie die maximale Bildbreite überschreiten',
 
 	'attributes_size_height' => 'Maximale Bildhöhe',
-	'attributes_size_height_explain' => 'Bilder werden angepasst wenn sie die maximale Bildhöhe überschreiten',
+	'attributes_size_height_explain' => 'Bilder werden angepasst, wenn sie die maximale Bildhöhe überschreiten',
 
 	// custom vars
 	'name' => 'Name',
-	'name_explain' => 'Ein unique Name',
-	'name_missing' => 'Bitte einen unique Name angeben',
+	'name_explain' => 'Ein eindeutiger Name',
+	'name_missing' => 'Bitte einen eindeutigen Namen angeben',
 	'name_exists' => 'Der Name ist leider schon in Benutzung',
 
 	'value' => 'Wert',
-	'value_explain' => 'Die Daten die du speichern willst (bis zu 64KB)',
-	'value_code_snipet' => 'Snippet das in dein Template eingefügt werden soll:<br>
+	'value_explain' => 'Die Daten, die diese Variable enthalten soll (maximal 64KB)',
+	'value_code_snipet' => 'Code, um diese Variable einzufügen:<br>
 		<code>' . e('<?php echo site_meta(\'%s\'); ?>') . '</code>',
 
 	// messages
 	'variable_created' => 'Deine Variable wurde erstellt',
 	'variable_updated' => 'Deine Variable wurde geändert',
 	'variable_deleted' => 'Deine Variable wurde gelöscht',
+
+	'pagetype_created' => 'Dein Seitentyp wurde erstellt',
+	'pagetype_updated' => 'Dein Seitentyp wurde geändert',
+	'pagetype_deleted' => 'Dein Seitentyp wurde gelöscht',
 
 	'field_created' => 'Dein Feld wurde erstellt',
 	'field_updated' => 'Dein Feld wurde bearbeitet',
