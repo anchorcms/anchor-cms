91d260c87f4f896ad8523e57f31ec79a16f569a2
Kieron <info@kieronwilson.co.uk>
2015-12-10 11:27:28 +0000

    fix installer

--- app/start.php
+++ app/start.php
@@ -65,7 +65,7 @@ $app['errors']->register();
 $app['kernel']->redirectTrailingSlash();
 
 if(false === $app['installer']->isInstalled() || true === $app['installer']->installerRunning()) {
-	$app['router']->setRoutes(require __DIR__ . '/installer_routes.php');
+	$app['routes']->set(require __DIR__ . '/installer_routes.php');
 }
 else {
 	if( ! $app['config']->get('app.debug')) {
