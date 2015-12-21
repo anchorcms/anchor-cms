af9e4b88e1ad58699ca26d04bd3d26cc23bf49c5
Hariadi Hinta <diperakui@yahoo.com>
2015-12-21 08:43:06 +0800

    Installer: fix and update MySQL schema

--- app/storage/schema_mysql.sql
+++ app/storage/schema_mysql.sql
@@ -1,88 +1,92 @@
 CREATE TABLE `{prefix}categories` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`title` VARCHAR NOT NULL,
-	`slug` VARCHAR NOT NULL,
-	`description` TEXT NOT NULL,
-	PRIMARY KEY (`id`)
+	`id` int(6) NOT NULL AUTO_INCREMENT,
+	`title` varchar(150) NOT NULL,
+	`slug` varchar(40) NOT NULL,
+	`description` text NOT NULL,
+	PRIMARY KEY (`id`),
+	KEY `{prefix}categories_slug` (`slug`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `{prefix}extend` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`type` TEXT NOT NULL,
-	`field` TEXT NOT NULL,
-	`key` VARCHAR NOT NULL,
-	`label` VARCHAR NOT NULL,
-	`attributes` TEXT NOT NULL,
-	PRIMARY KEY (`id`)
+	`id` int(6) NOT NULL AUTO_INCREMENT,
+	`type` enum('post','page','user') NOT NULL,
+	`field` enum('text','html','image','file') NOT NULL,
+	`key` varchar(160) NOT NULL,
+	`label` varchar(160) NOT NULL,
+	`attributes` text NOT NULL,
+	PRIMARY KEY (`id`),
+	KEY `{prefix}extend_type` (`type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `{prefix}meta` (
-	`key` VARCHAR NOT NULL,
-	`value` TEXT NOT NULL,
+	`key` varchar(140) NOT NULL,
+	`value` text NOT NULL,
 	PRIMARY KEY (`key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `{prefix}page_meta` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`page` INT NOT NULL,
-	`extend` INT NOT NULL,
-	`data` TEXT NOT NULL,
+	`id` int(6) NOT NULL AUTO_INCREMENT,
+	`page` int(6) NOT NULL,
+	`extend` int(6) NOT NULL,
+	`data` text NOT NULL,
 	PRIMARY KEY (`id`),
-	KEY `{prefix}page_meta_extend` (`extend`),
-	KEY `{prefix}page_meta_page` (`page`)
+	KEY `{prefix}page_meta_page` (`page`),
+	KEY `{prefix}page_meta_extend` (`extend`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `{prefix}pages` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`parent` INT NOT NULL,
-	`slug` VARCHAR NOT NULL,
-	`name` VARCHAR NOT NULL,
-	`title` VARCHAR NOT NULL,
-	`content` TEXT NOT NULL,
-	`html` TEXT NOT NULL,
-	`status` TEXT NOT NULL,
-	`redirect` TEXT NOT NULL,
-	`show_in_menu` INT NOT NULL,
-	`menu_order` INT NOT NULL,
+	`id` int(6) NOT NULL AUTO_INCREMENT,
+	`parent` int(6) NOT NULL DEFAULT '0',
+	`slug` varchar(150) NOT NULL,
+	`name` varchar(64) NOT NULL,
+	`title` varchar(150) NOT NULL,
+	`content` text NOT NULL,
+	`html` text NOT NULL,
+	`status` enum('draft','published','archived') NOT NULL,
+	`redirect` text NOT NULL,
+	`show_in_menu` tinyint(1) NOT NULL,
+	`menu_order` int(4) NOT NULL DEFAULT 0,
 	PRIMARY KEY (`id`),
-	KEY `{prefix}pages_slug` (`slug`),
-	KEY `{prefix}pages_status` (`status`)
+	KEY `{prefix}pages_status` (`status`),
+	KEY `{prefix}pages_slug` (`slug`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `{prefix}post_meta` (
+	`id` int(6) NOT NULL AUTO_INCREMENT,
+	`post` int(6) NOT NULL,
+	`extend` int(6) NOT NULL,
+	`data` text NOT NULL,
+	PRIMARY KEY (`id`),
+	KEY `{prefix}post_meta_post` (`post`),
+	KEY `{prefix}post_meta_extend` (`extend`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `{prefix}posts` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`title` VARCHAR NOT NULL,
-	`slug` VARCHAR NOT NULL,
-	`content` TEXT NOT NULL,
-	`html` TEXT NOT NULL,
-	`created` DATETIME NOT NULL,
-	`modified` DATETIME NOT NULL,
-	`author` INT NOT NULL,
-	`category` INT NOT NULL,
-	`status` TEXT NOT NULL,
+	`id` int(6) NOT NULL AUTO_INCREMENT,
+	`title` varchar(150) NOT NULL,
+	`slug` varchar(150) NOT NULL,
+	`content` text NOT NULL,
+	`html` text NOT NULL,
+	`created` datetime NOT NULL,
+	`modified` datetime NOT NULL,
+	`author` int(6) NOT NULL,
+	`category` int(6) NOT NULL,
+	`status` enum('draft','published','archived') NOT NULL,
 	PRIMARY KEY (`id`),
 	KEY `{prefix}posts_slug` (`slug`),
 	KEY `{prefix}posts_status` (`status`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `{prefix}post_meta` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`post` INT NOT NULL,
-	`extend` INT NOT NULL,
-	`data` TEXT NOT NULL,
-	PRIMARY KEY (`id`),
-	KEY `{prefix}post_meta_extend` (`extend`),
-	KEY `{prefix}post_meta_post` (`post`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 CREATE TABLE `{prefix}users` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`username` VARCHAR NOT NULL,
-	`password` TEXT NOT NULL,
-	`email` VARCHAR NOT NULL,
-	`real_name` VARCHAR NOT NULL,
-	`bio` TEXT NOT NULL,
-	`status` TEXT NOT NULL,
-	`role` TEXT NOT NULL,
-	PRIMARY KEY (`id`)
+	`id` int(6) NOT NULL AUTO_INCREMENT,
+	`username` varchar(100) NOT NULL,
+	`password` text NOT NULL,
+	`email` varchar(140) NOT NULL,
+	`real_name` varchar(140) NOT NULL,
+	`bio` text NOT NULL,
+	`status` enum('inactive','active') NOT NULL,
+	`role` enum('admin','editor','user') NOT NULL,
+	PRIMARY KEY (`id`),
+	KEY `{prefix}users_status` (`status`),
+	KEY `{prefix}users_usernames` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
