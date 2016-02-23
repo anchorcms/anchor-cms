50e0497ee734a64c5a33871e54fe63894971de06
CraigChilds94 <childscraig17@gmail.com>
2016-02-23 13:11:02 +0000

    Alter default text in welcome panel

--- anchor/views/panel.php
+++ anchor/views/panel.php
@@ -5,7 +5,9 @@
 </hgroup>
 
 <section class="wrap">
-	<h3><?php echo __('panel.welcome', 'Welcome'); ?></h3>
+	<h3><?php echo __('panel.welcome', 'Welcome'); ?> to your Anchor site.</h3>
+	</br>
+	<p>Here you will find all of the tools you will need to produce content for your website, manage users, posts and pages. For more information on what you can do with Anchor, please see our documentation at <a href="https://anchorcms.com/docs">http://anchorcms.com/docs</a></p>
 </section>
 
 <?php echo $footer; ?>
