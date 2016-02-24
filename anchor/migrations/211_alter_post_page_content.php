12cea7b03478cd961ab9a4632046dd9918b81ce4
CraigChilds94 <childscraig17@gmail.com>
2016-02-24 20:29:44 +0000

    Make fixes on latest migration

--- anchor/migrations/211_alter_post_page_content.php
+++ anchor/migrations/211_alter_post_page_content.php
@@ -4,6 +4,7 @@ class Migration_alter_post_page_content extends Migration {
 
     public function up() {
         $table = Base::table('pages');
+        $table2 = Base::table('posts');
 
         if($this->has_table_column($table, 'content')) {
             $sql  = 'ALTER TABLE `' . $table . '` ';
@@ -11,15 +12,13 @@ class Migration_alter_post_page_content extends Migration {
             DB::ask($sql);
         }
 
-        if($this->has_table_column($table, 'markdown')) {
+        if(!$this->has_table_column($table, 'html') && $this->has_table_column($table, 'markdown')) {
             $sql  = 'ALTER TABLE `' . $table . '` ';
             $sql .= 'ADD `html` TEXT NOT NULL AFTER `markdown`';
             DB::ask($sql);
         }
 
-        $table2 = Base::table('posts');
-
-        if($this->has_table_column($table2, 'html')) {
+        if(!$this->has_table_column($table2, 'markdown') && $this->has_table_column($table2, 'html')) {
             $sql  = 'ALTER TABLE `' . $table2 . '` ';
             $sql .= 'ADD `markdown` TEXT NOT NULL AFTER `description`';
             DB::ask($sql);
