392e918806316565cb7df72f76c7553c73a9be2d
CraigChilds94 <childscraig17@gmail.com>
2016-02-25 23:44:43 +0000

    Remove auto-composer install since people can now use packagist

--- anchor/composer_check.php
+++ anchor/composer_check.php
@@ -1,11 +1,6 @@
 <?php 
 
-// Try to composer install
+// If that didn't work then produce an error
 if (!file_exists(PATH . 'vendor')) {
-    @exec('composer install');
-    
-    // If that didn't work then produce an error
-    if (!file_exists(PATH . 'vendor')) {
-      die('<code>We were unable to run composer our selves. Please run "composer install" from the command line to install Anchor. If you do not have composer installed please see <a href="https://getcomposer.org/">https://getcomposer.org/</a></code>');
-    }
+  die('<code>We were unable to run composer our selves. Please run "composer install" from the command line to install Anchor. If you do not have composer installed please see <a href="https://getcomposer.org/">https://getcomposer.org/</a></code>');
 }
