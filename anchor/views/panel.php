408bb6dbe2d02f9311143354b86f96ddaa1230f2
Nicolò Altamura <nicoalta01@gmail.com>
2016-05-27 20:43:18 +0200

    Update panel.php

--- anchor/views/panel.php
+++ anchor/views/panel.php
@@ -7,7 +7,8 @@
 <section class="wrap">
 	<h3><?php echo __('panel.title', 'Welcome'); ?></h3>
 	</br>
-	<p><?php echo __('panel.message', 'For more information see <a href="https://anchorcms.com/docs">http://anchorcms.com/docs</a>'); ?></p>
+	<p><?php echo __('panel.message', 'For more information see <a href="https://anchorcms.com/docs">http://anchorcms.com/docs</a>'); ?></p> <br>
+	<?php echo __('panel.screenshot', 'Screenshot'); ?>
 </section>
 
 <?php echo $footer; ?>
