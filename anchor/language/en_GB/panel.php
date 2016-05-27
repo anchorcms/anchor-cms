90e1958923ef6c854ab8648e48b4d72689aa3554
Nicolò Altamura <nicoalta01@gmail.com>
2016-05-27 20:43:09 +0200

    Update panel.php

--- anchor/language/en_GB/panel.php
+++ anchor/language/en_GB/panel.php
@@ -5,5 +5,5 @@ return array(
     'panel' => 'Administration Panel',
     'title' => 'Welcome to your Anchor site',
     'message' => 'Here you will find all of the tools you will need to produce content for your website, manage users, posts and pages. For more information on what you can do with Anchor, please see our documentation at <a href="https://anchorcms.com/docs">http://anchorcms.com/docs</a>',
-    
+    'screenshot' => '<img src="anchor/views/assets/img/post-new.png"'
 );
