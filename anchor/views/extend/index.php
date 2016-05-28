b15a484fb17867c5cf478dbd24cb7b87fb6e36c5
Nicolò Altamura <nicoalta01@gmail.com>
2016-05-28 21:18:33 +0200

    Add a simple icon

--- anchor/views/extend/index.php
+++ anchor/views/extend/index.php
@@ -10,6 +10,7 @@
 	<ul class="list">
 		<li>
 			<a href="<?php echo Uri::to('admin/extend/pagetypes'); ?>">
+				<i class="fa fa-3x fa-anchor pull-left wow " data-wow-delay=".3s" style="visibility: visible; animation-delay: 0.3s; animation-name: bounceIn;"> </i>
 				<strong><?php echo __('extend.pagetypes'); ?></strong>
 				<span><?php echo __('extend.pagetypes_desc'); ?></span>
 			</a>
