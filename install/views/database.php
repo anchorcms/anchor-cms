93748c1f1ef9f34efb522ff1d8fe0b49d37d43ef
Gerrit0 <gerrit@gerritbirkeland.com>
2016-06-06 15:03:03 -0600

    Fix anchorcms/anchor-cms#1096
    1. Fix validator bug where if the value was null, then checks would
    *always* pass.
    2.  Catch Exceptions, rather than PDOExceptions when trying to connect
    to the database for the first time to prevent connection errors being
    displayed in the error page.
    3. Add Notify::read() to install views so that the user knows what went
    wrong.

--- install/views/database.php
+++ install/views/database.php
@@ -5,10 +5,12 @@
 		<h1>Your database details</h1>
 
 		<p>Firstly, we’ll need a database. Anchor needs them to store all of your blog’s information, so it’s vital you fill these in correctly. If you don’t know what these are, you’ll need to contact your webhost.</p>
+
+		<?php echo Notify::read(); ?>
 	</article>
 
 	<form method="post" action="<?php echo uri_to('database'); ?>" autocomplete="off">
-		
+
 
 		<fieldset>
 			<p>
