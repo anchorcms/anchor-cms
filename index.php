d4b3d003b606f5a9c7f094f4d443697a5cb2dc1e
Johannes Konst <info@johanneskonst.nl>
2017-02-01 13:03:33 +0100

    Since we require 5.3 already let's use constants that are available to us, like __DIR__

--- index.php
+++ index.php
@@ -22,11 +22,11 @@
 */
 
 define('DS', DIRECTORY_SEPARATOR);
-define('ENV', getenv('APP_ENV'));
+define('ENV', getenv('APP_ENV') || 'dev');
 define('VERSION', '0.12.1');
 define('MIGRATION_NUMBER', 213);
 
-define('PATH', dirname(__FILE__) . DS);
+define('PATH', __DIR__ . DS);
 define('APP', PATH . 'anchor' . DS);
 define('SYS', PATH . 'system' . DS);
 define('EXT', '.php');
--- install/index.php
+++ install/index.php
@@ -25,7 +25,7 @@ define('DS', DIRECTORY_SEPARATOR);
 define('ENV', getenv('APP_ENV'));
 define('VERSION', '0.12.1');
 
-define('PATH', dirname(dirname(__FILE__)) . DS);
+define('PATH', dirname(__DIR__) . DS);
 define('APP', PATH . 'install' . DS);
 define('SYS', PATH . 'system' . DS);
 define('EXT', '.php');
