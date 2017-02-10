d212d40f396582c79b88c7cdfb85064cfd8a46e1
Jarod Brennfleck <robinjars@gmail.com>
2017-02-10 19:30:08 +0930

    updated migration number

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 define('DS', DIRECTORY_SEPARATOR);
 define('ENV', getenv('APP_ENV'));
 define('VERSION', '0.12.1');
-define('MIGRATION_NUMBER', 213);
+define('MIGRATION_NUMBER', 220);
 
 define('PATH', __DIR__ . DS);
 define('APP', PATH . 'anchor' . DS);
