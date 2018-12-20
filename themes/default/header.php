b5adfcd834ea4a4879d324ad30282579cd258fc8
David Apple <davidapple@protonmail.com>
2018-12-20 11:15:06 +0700

    Revert "Space to tabs in default theme"
    This reverts commit 4d967d315faf86cf9f53806b5efc7a91fb53a2ed.

--- themes/default/header.php
+++ themes/default/header.php
@@ -21,22 +21,22 @@
 		<script src="<?php echo asset_url('/js/zepto.js'); ?>"></script>
 		<script src="<?php echo theme_url('/js/main.js'); ?>"></script>
 
-		<meta name="viewport" content="width=device-width">
-		<meta name="generator" content="Anchor CMS">
+	    <meta name="viewport" content="width=device-width">
+	    <meta name="generator" content="Anchor CMS">
 
-		<meta property="og:title" content="<?php echo page_title(); ?>">
-		<meta property="og:type" content="website">
-		<meta property="og:url" content="<?php echo e(current_url()); ?>">
-		<meta property="og:image" content="<?php echo theme_url('img/og_image.gif'); ?>">
-		<meta property="og:site_name" content="<?php echo site_name(); ?>">
-		<meta property="og:description" content="<?php echo page_description(); ?>">
+	    <meta property="og:title" content="<?php echo page_title(); ?>">
+	    <meta property="og:type" content="website">
+	    <meta property="og:url" content="<?php echo e(current_url()); ?>">
+	    <meta property="og:image" content="<?php echo theme_url('img/og_image.gif'); ?>">
+	    <meta property="og:site_name" content="<?php echo site_name(); ?>">
+	    <meta property="og:description" content="<?php echo page_description(); ?>">
 
 		<?php if (customised()): ?>
-			<!-- Custom CSS -->
-			<style><?php echo article_css(); ?></style>
+		    <!-- Custom CSS -->
+    		<style><?php echo article_css(); ?></style>
 
-			<!--  Custom Javascript -->
-			<script><?php echo article_js(); ?></script>
+    		<!--  Custom Javascript -->
+    		<script><?php echo article_js(); ?></script>
 		<?php endif; ?>
 	</head>
 	<body class="<?php echo body_class(); ?>">
@@ -53,19 +53,19 @@
 						<b>Categories</b>
 						<ul>
 						<?php while (categories()):
-								if (category_count() > 0) {
-									?>
+                                if (category_count() > 0) {
+                                    ?>
 							<li>
 								<a href="<?php echo category_url();
-									?>" title="<?php echo category_description();
-									?>">
+                                    ?>" title="<?php echo category_description();
+                                    ?>">
 									<?php echo category_title();
-									?> <span><?php echo category_count();
-									?></span>
+                                    ?> <span><?php echo category_count();
+                                    ?></span>
 								</a>
 							</li>
 						<?php 
-								} endwhile; ?>
+                                } endwhile; ?>
 						</ul>
 					</aside>
 				</div>
@@ -77,14 +77,14 @@
 				<nav id="main" role="navigation">
 					<ul>
 						<?php if (has_menu_items()):
-							while (menu_items()): ?>
+                            while (menu_items()): ?>
 						<li <?php echo(menu_active() ? 'class="active"' : ''); ?>>
 							<a href="<?php echo menu_url(); ?>" title="<?php echo menu_title(); ?>">
 								<?php echo menu_name(); ?>
 							</a>
 						</li>
 						<?php endwhile;
-							endif; ?>
+                            endif; ?>
 						<li class="tray">
 							<a href="#tray" class="linky"><img src="<?php echo theme_url('img/categories.png'); ?>" alt="Categories" title="View my posts by category"></a>
 						</li>
