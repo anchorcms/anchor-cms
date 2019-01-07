35c387c93f6399e438f993cd92d967bea9e9490a
Elton Mesquita <callmeelton@gmail.com>
2019-01-07 17:35:32 -0300

    Add image draganddrop to Pages - Fixes #1304

--- anchor/views/pages/edit.php
+++ anchor/views/pages/edit.php
@@ -118,6 +118,7 @@
 </form>
 
 <script src="<?php echo asset('anchor/views/assets/js/redirect.js'); ?>"></script>
+<script src="<?php echo asset('anchor/views/assets/js/dragdrop.js'); ?>"></script>
 <script src="<?php echo asset('anchor/views/assets/js/upload-fields.js'); ?>"></script>
 <script src="<?php echo asset('anchor/views/assets/js/text-resize.js'); ?>"></script>
 <script src="<?php echo asset('anchor/views/assets/js/editor.js'); ?>"></script>
